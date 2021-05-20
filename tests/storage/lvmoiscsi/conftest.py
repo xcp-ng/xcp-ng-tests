886650bd98628270ed8973e4d6f91e0cd54040e5
Ronan Abhamon <ronan.abhamon@vates.fr>
2021-05-20 17:37:45 +0200

    Add LVMoISCSI tests
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

---
+++ tests/storage/lvmoiscsi/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+@pytest.fixture(scope='session')
+def lvmoiscsi_device_config(sr_device_config):
+    if sr_device_config is not None:
+        # SR device config from CLI param
+        config = sr_device_config
+    else:
+        # SR device config from data.py defaults
+        try:
+            from data import DEFAULT_LVMOISCSI_DEVICE_CONFIG
+        except ImportError:
+            DEFAULT_LVMOISCSI_DEVICE_CONFIG = {}
+        if DEFAULT_LVMOISCSI_DEVICE_CONFIG:
+            config = DEFAULT_LVMOISCSI_DEVICE_CONFIG
+        else:
+            raise Exception("No default lvmoiscsi device-config found, neither in CLI nor in data.py defaults")
+    return config
+
+@pytest.fixture(scope='session')
+def lvmoiscsi_sr(host, lvmoiscsi_device_config):
+    """ A lvmoiscsi SR on first host. """
+    sr = host.sr_create('lvmoiscsi', "lvmoiscsi-SR", lvmoiscsi_device_config, shared=True)
+    yield sr
+    # teardown
+    sr.destroy()
+
+@pytest.fixture(scope='module')
+def vm_on_lvmoiscsi_sr(host, lvmoiscsi_sr, vm_ref):
+    print(">> ", end='')
+    vm = host.import_vm(vm_ref, sr_uuid=lvmoiscsi_sr.uuid)
+    yield vm
+    # teardown
+    print("<< Destroy VM")
+    vm.destroy(verify=True)
---
+++ tests/storage/lvmoiscsi/test_lvmoiscsi_sr.py
@@ -0,0 +1,53 @@
+import pytest
+import subprocess
+from lib.common import wait_for, vm_image
+
+# Requirements:
+# - one XCP-ng host >= 8.2
+# - a valid lvmoiscsi config
+
+class TestLVMOISCSISRCreateDestroy:
+    """
+    Tests that do not use fixtures that setup the SR or import VMs,
+    because they precisely need to test SR creation and destruction,
+    and VM import.
+    """
+
+    def test_create_and_destroy_sr(self, host, lvmoiscsi_device_config):
+        # Create and destroy tested in the same test to leave the host as unchanged as possible
+        sr = host.sr_create('lvmoiscsi', "lvmoiscsi-SR", lvmoiscsi_device_config, shared=True, verify=True)
+        # import a VM in order to detect vm import issues here rather than in the vm_on_xfs_fixture used in
+        # the next tests, because errors in fixtures break teardown
+        vm = host.import_vm(vm_image('mini-linux-x86_64-bios'), sr_uuid=sr.uuid)
+        vm.destroy(verify=True)
+        sr.destroy(verify=True)
+
+
+@pytest.mark.usefixtures("lvmoiscsi_sr", "vm_on_lvmoiscsi_sr")
+class TestLVMOISCSISR:
+    def test_start_and_shutdown_VM(self, vm_on_lvmoiscsi_sr):
+        vm = vm_on_lvmoiscsi_sr
+        vm.start()
+        vm.wait_for_os_booted()
+        vm.shutdown(verify=True)
+
+    def test_snapshot(self, vm_on_lvmoiscsi_sr):
+        vm = vm_on_lvmoiscsi_sr
+        vm.start()
+        vm.wait_for_os_booted()
+        vm.test_snapshot_on_running_vm()
+        vm.shutdown(verify=True)
+
+    # *** tests with reboots (longer tests).
+
+    def test_reboot(self, host, lvmoiscsi_sr, vm_on_lvmoiscsi_sr):
+        sr = lvmoiscsi_sr
+        vm = vm_on_lvmoiscsi_sr
+        host.reboot(verify=True)
+        wait_for(sr.all_pbds_attached, "Wait for PDB attached")
+        # start the VM as a way to check that the underlying SR is operational
+        vm.start()
+        vm.wait_for_os_booted()
+        vm.shutdown(verify=True)
+
+    # *** End of tests with reboots
