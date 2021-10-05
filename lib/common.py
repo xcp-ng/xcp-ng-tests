xcp-ng/xcp-ng-tests

commit 9524dcbc9a0d282f3f11f7b84d682b6815e1a09c

Use specific structs/Exceptions for ssh commands + remove banner messages in stdout
Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

lib/common.py
@@ -5,7 +5,6 @@ import subprocess
 import tempfile
 import time
 from enum import Enum
-from subprocess import CalledProcessError
 from uuid import UUID
 
 from lib.efi import EFIAuth, EFI_HEADER_MAGIC
@@ -65,8 +64,23 @@ def xo_object_exists(uuid):
     lst = json.loads(xo_cli('--list-objects', {'uuid': uuid}))
     return len(lst) > 0
 
-class SSHException(Exception):
-    pass
+class SSHCommandFailed(Exception):
+    __slots__ = 'returncode', 'stdout', 'cmd'
+
+    def __init__(self, returncode, stdout, cmd):
+        super(SSHCommandFailed, self).__init__(
+            'SSH command ({}) failed with return code {}: {}'.format(cmd, returncode, stdout)
+        )
+        self.returncode = returncode
+        self.stdout = stdout
+        self.cmd = cmd
+
+class SSHResult:
+    __slots__ = 'returncode', 'stdout'
+
+    def __init__(self, returncode, stdout):
+        self.returncode = returncode
+        self.stdout = stdout
 
 def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
         background=False, target_os='linux'):
@@ -96,22 +110,36 @@ def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprin
         )
     else:
         # Common case
+
+        # Fetch banner and remove it to avoid stdout/stderr pollution.
+        banner_res = subprocess.run(
+            "ssh root@%s %s '%s'" % (hostname_or_ip, ' '.join(options), '\n'),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            check=False
+        )
         res = subprocess.run(
             "ssh root@%s %s '%s'" % (hostname_or_ip, ' '.join(options), command),
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            check=check
+            check=False
         )
 
         # Even if check is False, we still raise in case of return code 255, which means a SSH error.
         if res.returncode == 255:
-            raise SSHException("SSH Error: %s" % res.stdout.decode())
+            raise SSHCommandFailed(255, "SSH Error: %s" % res.stdout.decode(), command)
+        if banner_res.returncode == 255:
+            raise SSHCommandFailed(255, "SSH Error: %s" % banner_res.stdout.decode(), command)
+
+        stdout = res.stdout.decode()[len(banner_res.stdout.decode()):]
+        if check and res.returncode:
+            raise SSHCommandFailed(res.returncode, stdout, command)
 
         if simple_output:
-            return res.stdout.decode().strip()
-        else:
-            return res
+            return stdout.strip()
+        return SSHResult(res.returncode, stdout)
 
 def scp(hostname_or_ip, src, dest, check=True, suppress_fingerprint_warnings=True, local_dest=False):
     options = ""
@@ -126,13 +154,17 @@ def scp(hostname_or_ip, src, dest, check=True, suppress_fingerprint_warnings=Tru
     else:
         dest = 'root@{}:{}'.format(hostname_or_ip, dest)
 
+    command = "scp {} {} {}".format(options, src, dest)
     res = subprocess.run(
-        "scp {} {} {}".format(options, src, dest),
+        command,
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
-        check=check
+        check=False
     )
+    if check and res.returncode:
+        raise SSHCommandFailed(res.returncode, res.stdout.decode(), command)
+
     return res
 
 def to_xapi_bool(b):
@@ -340,7 +372,7 @@ class Host:
     def is_enabled(self):
         try:
             return self.xe('host-param-get', {'uuid': self.uuid, 'param-name': 'enabled'})
-        except subprocess.CalledProcessError:
+        except SSHCommandFailed:
             # If XAPI is not ready yet, or the host is down, this will throw. We return False in that case.
             return False
 
@@ -348,9 +380,9 @@ class Host:
         try:
             # yum check-update returns 100 if there are updates, 1 if there's an error, 0 if no updates
             self.ssh(['yum', 'check-update'])
-            # returned 0, else there would have been a CalledProcessError
+            # returned 0, else there would have been a SSHCommandFailed
             return False
-        except CalledProcessError as e:
+        except SSHCommandFailed as e:
             if e.returncode == 100:
                 return True
             else:
@@ -418,10 +450,10 @@ class Host:
         print("Reboot host %s" % self)
         try:
             self.ssh(['reboot'])
-        except subprocess.CalledProcessError as e:
+        except SSHCommandFailed as e:
             # ssh connection may get killed by the reboot and terminate with an error code
-            if "closed by remote host" in e.stdout.decode().strip():
-                pass
+            if "closed by remote host" not in e.stdout:
+                raise
         if verify or reconnect_xo:
             wait_for_not(self.is_enabled, "Wait for host down")
             wait_for(self.is_enabled, "Wait for host up", timeout_secs=600)
@@ -491,8 +523,8 @@ class BaseVM:
             args['param-key'] = key
         try:
             value = self.host.xe('vm-param-get', args)
-        except subprocess.CalledProcessError as e:
-            if key and accept_unknown_key and e.stdout.decode().strip() == "Error: Key %s not found in map" % key:
+        except SSHCommandFailed as e:
+            if key and accept_unknown_key and e.stdout.strip() == "Error: Key %s not found in map" % key:
                 value = None
             else:
                 raise
@@ -633,7 +665,7 @@ class VM(BaseVM):
     def is_ssh_up(self):
         try:
             return self.ssh_with_result(['true']).returncode == 0
-        except SSHException:
+        except SSHCommandFailed:
             # probably not up yet
             return False
 
@@ -967,7 +999,7 @@ class SR:
         for pbd_uuid in self.pbd_uuids():
             try:
                 self.pool.master.xe('pbd-unplug', {'uuid': pbd_uuid})
-            except subprocess.CalledProcessError as e:
+            except SSHCommandFailed as e:
                 # We must be sure to execute correctly "unplug" on unplugged VDIs without error
                 # if force is set.
                 if not force:

lib/efi.py
@@ -9,8 +9,6 @@ import os
 import shlex
 import shutil
 import struct
-import subprocess
-import uuid
 from datetime import datetime, timedelta
 from subprocess import check_call
 from tempfile import TemporaryDirectory

tests/migration/test_host_evacuate.py
@@ -1,7 +1,6 @@
 import pytest
 
-from subprocess import CalledProcessError
-from lib.common import wait_for, wait_for_not
+from lib.common import SSHCommandFailed, wait_for, wait_for_not
 # The pool needs a shared SR to use `host.evacuate`
 from tests.storage.nfs.conftest import vm_on_nfs_sr, nfs_sr, nfs_device_config
 
@@ -30,8 +29,8 @@ def _host_evacuate_test(source_host, dest_host, network_uuid, vm, expect_error=F
         wait_for(lambda: vm.is_running_on_host(dest_host), "Wait for VM to be running on destination host")
         vm.wait_for_os_booted()
         assert not expect_error, "host-evacuate should have raised: %s" % error
-    except CalledProcessError as e:
-        if not (expect_error and e.output.find(error) > -1):
+    except SSHCommandFailed as e:
+        if not (expect_error and e.stdout.find(error) > -1):
             raise
     finally:
         vm.shutdown(verify=True)

tests/storage/cephfs/test_cephfs_sr.py
@@ -1,7 +1,6 @@
 import pytest
-from lib.common import wait_for, vm_image
+from lib.common import SSHCommandFailed, wait_for, vm_image
 import time
-import subprocess
 
 # Requirements:
 # - one XCP-ng host >= 8.2
@@ -74,7 +73,7 @@ class TestCephFSSR:
             try:
                 sr.scan()
                 assert False, "SR scan should have failed"
-            except subprocess.CalledProcessError:
+            except SSHCommandFailed:
                 print("SR scan failed as expected.")
             host.reboot(verify=True)
             # give the host some time to try to attach the SR

tests/storage/linstor/test_linstor_sr.py
@@ -1,6 +1,5 @@
 from conftest import GROUP_NAME, create_linstor_sr, destroy_linstor_sr
-from lib.common import wait_for, vm_image
-from subprocess import CalledProcessError
+from lib.common import SSHCommandFailed, wait_for, vm_image
 import pytest
 import time
 
@@ -30,7 +29,7 @@ class TestLinstorSRCreateDestroy:
             except Exception:
                 pass
             assert False, "SR creation should not have succeeded!"
-        except CalledProcessError as e:
+        except SSHCommandFailed as e:
             print("SR creation failed, as expected: {}".format(e))
 
     def test_create_and_destroy_sr(self, hosts_with_linstor, lvm_disks):
@@ -80,7 +79,7 @@ class TestLinstorSR:
             try:
                 sr.scan()
                 assert False, "SR scan should have failed"
-            except CalledProcessError:
+            except SSHCommandFailed:
                 print("SR scan failed as expected.")
             host.reboot(verify=True)
             # give the host some time to try to attach the SR

tests/storage/moosefs/test_moosefs_sr.py
@@ -1,6 +1,5 @@
 import pytest
-from lib.common import wait_for, vm_image
-import subprocess
+from lib.common import SSHCommandFailed, wait_for, vm_image
 import time
 
 # Requirements:
@@ -75,7 +74,7 @@ class TestMooseFSSR:
             try:
                 sr.scan()
                 assert False, "SR scan should have failed"
-            except subprocess.CalledProcessError:
+            except SSHCommandFailed:
                 print("SR scan failed as expected.")
             host.reboot(verify=True)
             # give the host some time to try to attach the SR

tests/storage/storage.py
@@ -1,13 +1,12 @@
-from lib.common import wait_for
-import subprocess
+from lib.common import SSHCommandFailed, wait_for
 
 def try_to_create_sr_with_missing_device(sr_type, label, host):
     try:
         sr = host.sr_create(sr_type, label, {}, verify=True)
-    except subprocess.CalledProcessError as e:
+    except SSHCommandFailed as e:
         assert e.stdout == (
-            b'Error code: SR_BACKEND_FAILURE_90\nError parameters: , '
-            b'The request is missing the device parameter, \n'
+            'Error code: SR_BACKEND_FAILURE_90\nError parameters: , '
+            + 'The request is missing the device parameter, \n'
         ), 'Bad error, current: {}'.format(e.stdout)
         return
     assert False, 'SR creation should not have succeeded!'

tests/storage/xfs/test_xfs_sr.py
@@ -1,7 +1,6 @@
 import pytest
-from lib.common import wait_for, vm_image
+from lib.common import SSHCommandFailed, wait_for, vm_image
 import time
-import subprocess
 
 # Requirements:
 # - one XCP-ng host >= 8.2 with an additional unused disk for the SR
@@ -73,7 +72,7 @@ class TestXFSSR:
             try:
                 sr.scan()
                 assert False, "SR scan should have failed"
-            except subprocess.CalledProcessError:
+            except SSHCommandFailed:
                 print("SR scan failed as expected.")
             host.reboot(verify=True)
             # give the host some time to try to attach the SR

commit 25d05b9d4e0787728ce55d0aea9ae01699a878bc

Fix secureboot test regression due to output decoding
Many tests from tests/uefistored/test_secure_boot.py are failing with an
utf-8 decoding error happening in the VM.get_all_efi_bins method.

This is a regression introduced by commit
9524dcbc9a0d282f3f11f7b84d682b6815e1a09c which added a systematic
decoding of SSH command output. This doesn't play well will
VM.get_all_efi_bins which outputs the first two bytes of various binary
files, so this can't always be decoded as text.

Add a `decode` boolean parameter to the ssh functions, defaulting to True
but which when set to False will cause the output not to be decoded
(except for logging and error reporting, by setting errors='replace' in
order to be fault-tolerant).

Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

lib/common.py
@@ -98,7 +98,7 @@ def _ellide_log_lines(log):
     return "\n{}".format("\n".join(reduced_message))
 
 def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
-        background=False, target_os='linux'):
+        background=False, target_os='linux', decode=True):
     options = []
     if suppress_fingerprint_warnings:
         # Suppress warnings and questions related to host key fingerprints
@@ -143,29 +143,34 @@ def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprin
             check=False
         )
 
+        # get a decoded version of the output in any case, replacing potential errors
+        output_for_logs = res.stdout.decode(errors='replace').strip()
+
         # Even if check is False, we still raise in case of return code 255, which means a SSH error.
         if res.returncode == 255:
-            raise SSHCommandFailed(255, "SSH Error: %s" % res.stdout.decode(), command)
-        stdout = res.stdout.decode()
+            raise SSHCommandFailed(255, "SSH Error: %s" % output_for_logs, command)
 
+        output = res.stdout
         if config.ignore_ssh_banner:
             if banner_res.returncode == 255:
-                raise SSHCommandFailed(255, "SSH Error: %s" % banner_res.stdout.decode(), command)
-            stdout = stdout[len(banner_res.stdout.decode()):]
+                raise SSHCommandFailed(255, "SSH Error: %s" % banner_res.stdout.decode(errors='replace'), command)
+            output = output[len(banner_res.stdout):]
 
-        stripped_stdout = stdout.strip()
+        if decode:
+            output = output.decode()
+
         if res.returncode:
-            logging.debug("[{}] Got error code: {}, stdout: '{}' while running {}".format(
-                hostname_or_ip, res.returncode, stripped_stdout, command
+            logging.debug("[{}] Got error code: {}, output: '{}' while running {}".format(
+                hostname_or_ip, res.returncode, output_for_logs, command
             ))
             if check:
-                raise SSHCommandFailed(res.returncode, stdout, command)
+                raise SSHCommandFailed(res.returncode, output_for_logs, command)
         else:
-            logging.debug("[{}] {}".format(hostname_or_ip, command) + _ellide_log_lines(stripped_stdout))
+            logging.debug("[{}] {}".format(hostname_or_ip, command) + _ellide_log_lines(output_for_logs))
 
         if simple_output:
-            return stripped_stdout
-        return SSHResult(res.returncode, stdout)
+            return output.strip()
+        return SSHResult(res.returncode, output)
 
 def scp(hostname_or_ip, src, dest, check=True, suppress_fingerprint_warnings=True, local_dest=False):
     options = ""
@@ -259,9 +264,11 @@ class Host:
         else:
             self.pool = pool
 
-    def ssh(self, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True, background=False):
+    def ssh(self, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
+            background=False, decode=True):
         return ssh(self.hostname_or_ip, cmd, check=check, simple_output=simple_output,
-                   suppress_fingerprint_warnings=suppress_fingerprint_warnings, background=background)
+                   suppress_fingerprint_warnings=suppress_fingerprint_warnings, background=background,
+                   decode=decode)
 
     def ssh_with_result(self, cmd):
         # doesn't raise if the command's return is nonzero, unless there's a SSH error
@@ -683,10 +690,11 @@ class VM(BaseVM):
             self.ip = ip
             return True
 
-    def ssh(self, cmd, check=True, simple_output=True, background=False):
+    def ssh(self, cmd, check=True, simple_output=True, background=False, decode=True):
         # raises by default for any nonzero return code
         target_os = "windows" if self.is_windows else "linux"
-        return ssh(self.ip, cmd, check=check, simple_output=simple_output, background=background, target_os=target_os)
+        return ssh(self.ip, cmd, check=check, simple_output=simple_output, background=background,
+                   target_os=target_os, decode=decode)
 
     def ssh_with_result(self, cmd):
         # doesn't raise if the command's return is nonzero, unless there's a SSH error
@@ -973,7 +981,8 @@ class VM(BaseVM):
                 'do', 'echo', '$file', '$(head', '-c', magicsz, '$file);',
                 'done'
             ],
-            simple_output=False).stdout.split(b'\n')
+            simple_output=False,
+            decode=False).stdout.split(b'\n')
 
         magic = EFI_HEADER_MAGIC.encode('ascii')
         binaries = []