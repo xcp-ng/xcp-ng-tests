ded84cdf715d48cc9a565cef1e9b19ddbe96c8c9
Samuel Verschelde <stormi-xcp@ylix.fr>
2021-11-04 20:26:22 +0100

    Add tests/system/test_irqbalance.py to check VIF irq balancing
    Following a regression in a kernel hotfix, check that IRQs associated to
    VIFs are not all affected to the same CPU, to avoid any future similar
    regression.
    
    Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

--- lib/common.py
+++ lib/common.py
@@ -1092,6 +1092,25 @@ class VIF:
         self.uuid = uuid
         self.vm = vm
 
+    def param_get(self, param_name, key=None, accept_unknown_key=False):
+        args = {'uuid': self.uuid, 'param-name': param_name}
+        if key is not None:
+            args['param-key'] = key
+        try:
+            value = self.vm.host.xe('vif-param-get', args)
+        except commands.SSHCommandFailed as e:
+            if key and accept_unknown_key and e.stdout == "Error: Key %s not found in map" % key:
+                value = None
+            else:
+                raise
+        return value
+
+    def device_id(self):
+        """ Build the identifier that will allow to grep for the VIF's interrupts. """
+        dom_id = self.vm.param_get('dom-id')
+        device = self.param_get('device')
+        return f"vif{dom_id}.{device}"
+
     def move(self, network_uuid):
         self.vm.host.xe('vif-move', {'uuid': self.uuid, 'network-uuid': network_uuid})
 
