f98b9d7dd5c17caec1f64836d1b1512e9e3750bb
BenjiReis <benjamin.reis@vates.fr>
2022-01-18 16:06:54 +0100

    fix stream output with emptylines in the output
    Bug introduced in: c45eb5b0b1045a18da2c028ee88058cbf4a7568d
    
    Signed-off-by: BenjiReis <benjamin.reis@vates.fr>

--- lib/commands.py
+++ lib/commands.py
@@ -103,10 +103,9 @@ def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprin
     stdout = []
     for line in iter(process.stdout.readline, b''):
         readable_line = line.decode(errors='replace').strip()
-        if readable_line == '':
-            break
-        stdout.append(line)
-        OUPUT_LOGGER.debug(readable_line)
+        if readable_line != '':
+            stdout.append(line)
+            OUPUT_LOGGER.debug(readable_line)
     _, stderr = process.communicate()
     res = subprocess.CompletedProcess(ssh_cmd, process.returncode, b''.join(stdout), stderr)
 
