88cff6acb2717da25c190a2ddd41c7e0e2c29378
Samuel Verschelde <stormi-xcp@ylix.fr>
2022-03-30 12:34:18 +0200

    Add more log messages to host-evacuate tests
    Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

--- tests/migration/test_host_evacuate.py
+++ tests/migration/test_host_evacuate.py
@@ -28,7 +28,10 @@ def _host_evacuate_test(source_host, dest_host, network_uuid, vm, expect_error=F
         args['network-uuid'] = network_uuid
 
     try:
-        logging.info(f"Evacuate host {source_host}")
+        if not expect_error:
+            logging.info(f"Evacuate host {source_host}")
+        else:
+            logging.info(f"Attempt evacuating host {source_host}. This should fail.")
         source_host.xe('host-evacuate', args)
         wait_for(lambda: vm.all_vdis_on_host(dest_host), "Wait for all VDIs on destination host")
         wait_for(lambda: vm.is_running_on_host(dest_host), "Wait for VM to be running on destination host")
@@ -37,6 +40,7 @@ def _host_evacuate_test(source_host, dest_host, network_uuid, vm, expect_error=F
     except SSHCommandFailed as e:
         if not (expect_error and e.stdout.find(error) > -1):
             raise
+        logging.info(f"Evacuate failed with the expected error: {error}.")
     finally:
         vm.shutdown(verify=True)
 
@@ -69,21 +73,25 @@ class TestHostEvacuate:
         ipv6 = (host.xe('pif-param-get', {'uuid': pif_uuid, 'param-name': 'primary-address-type'}) == "IPv6")
         reconfigure_method = 'pif-reconfigure-ipv6' if ipv6 else 'pif-reconfigure-ip'
         args = _save_ip_configuration_mode(host, pif_uuid)
+        logging.info(f"Reconfigure PIF {pif_uuid}: remove its IP")
         host.xe(reconfigure_method, {'uuid': pif_uuid, 'mode': 'none'})
         try:
             no_ip_error = 'The specified interface cannot be used because it has no IP address'
             _host_evacuate_test(host, hostA2, second_network, vm_on_nfs_sr, True, no_ip_error)
         finally:
+            logging.info(f"Restore the configuration of PIF {pif_uuid}")
             host.xe(reconfigure_method, args)
 
     def test_host_evacuate_with_network_not_attached(self, host, hostA2, second_network, vm_on_nfs_sr):
         pif_uuid = host.xe('pif-list', {'host-uuid': host.uuid, 'network-uuid': second_network}, minimal=True)
+        logging.info(f"Unplug PIF {pif_uuid}")
         host.xe('pif-unplug', {'uuid': pif_uuid})
         try:
             not_attached_error = \
                 'The operation you requested cannot be performed because the specified PIF is currently unplugged'
             _host_evacuate_test(host, hostA2, second_network, vm_on_nfs_sr, True, not_attached_error)
         finally:
+            logging.info(f"Re-plug PIF {pif_uuid}")
             host.xe('pif-plug', {'uuid': pif_uuid})
 
     def test_host_evacuate_with_network_not_present(self, host, hostA2, second_network, vm_on_nfs_sr):
@@ -91,6 +99,7 @@ class TestHostEvacuate:
         ipv6 = (host.xe('pif-param-get', {'uuid': pif_uuid, 'param-name': 'primary-address-type'}) == "IPv6")
         reconfigure_method = 'pif-reconfigure-ipv6' if ipv6 else 'pif-reconfigure-ip'
         args = _save_ip_configuration_mode(host, pif_uuid)
+        logging.info(f"Forget PIF {pif_uuid}")
         host.xe('pif-forget', {'uuid': pif_uuid})
         try:
             not_present_error = 'This host has no PIF on the given network'
@@ -99,5 +108,6 @@ class TestHostEvacuate:
             host.xe('pif-scan', {'host-uuid': host.uuid})
             pif_uuid = host.xe('pif-list', {'host-uuid': host.uuid, 'network-uuid': second_network}, minimal=True)
             args['uuid'] = pif_uuid
+            logging.info(f"Re-add and plug PIF {pif_uuid}")
             host.xe(reconfigure_method, args)
             host.xe('pif-plug', {'uuid': pif_uuid})
