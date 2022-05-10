87a6b8561947409ab29f07a9e0ad023a73061b7c
Ronan Abhamon <ronan.abhamon@vates.fr>
2022-05-10 15:55:14 +0200

    Fix intrapool migration tests (MooseFS)
    - Remove useless `test_cold_intrapool_shared_migration` test
    - We must have MooseFS packages installed on all hosts + xapi.conf modified
    
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- tests/storage/moosefs/test_moosefs_sr_intrapool_migration.py
+++ tests/storage/moosefs/test_moosefs_sr_intrapool_migration.py
@@ -14,9 +14,6 @@ from tests.storage import cold_migration_then_come_back, live_storage_migration_
 @pytest.mark.big_vm # and ideally on a big VM to test it scales
 @pytest.mark.usefixtures("hostA2", "local_sr_on_hostA2")
 class Test:
-    def test_cold_intrapool_shared_migration(self, host, hostA2, vm_on_moosefs_sr, moosefs_sr):
-        cold_migration_then_come_back(vm_on_moosefs_sr, host, moosefs_sr, hostA2, moosefs_sr)
-
     def test_live_intrapool_shared_migration(self, host, hostA2, vm_on_moosefs_sr, moosefs_sr):
         live_storage_migration_then_come_back(vm_on_moosefs_sr, host, moosefs_sr, hostA2, moosefs_sr)
 
