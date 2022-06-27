xcp-ng/xcp-ng-tests

commit 0bec9b87a4d6df4c7f6dc9d13a7dd82e67dbf7e9

Run ssh and scp in batch mode + allow adding SSH options to ssh()
* Batch mode allows to avoid interactive questions such as a password
  prompt.
* Sometimes we may need to add options to our ssh calls, for example a
  smaller ConnectTimeout value.

Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

lib/commands.py
@@ -65,8 +65,8 @@ OUPUT_LOGGER.addHandler(OUTPUT_HANDLER)
 OUTPUT_HANDLER.setFormatter(logging.Formatter('%(message)s'))
 
 def _ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
-         background=False, target_os='linux', decode=True):
-    options = []
+         background=False, target_os='linux', decode=True, options=[]):
+    options.append('-o "BatchMode yes"')
     if suppress_fingerprint_warnings:
         # Suppress warnings and questions related to host key fingerprints
         # because on a test network IPs get reused, VMs are reinstalled, etc.
@@ -141,15 +141,15 @@ def _ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerpri
 # This function is kept short for shorter pytest traces upon SSH failures, which are common,
 # as pytest prints the whole function definition that raised the SSHCommandFailed exception
 def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
-        background=False, target_os='linux', decode=True):
+        background=False, target_os='linux', decode=True, options=[]):
     success, result_or_exc = _ssh(hostname_or_ip, cmd, check, simple_output, suppress_fingerprint_warnings,
-                                  background, target_os, decode)
+                                  background, target_os, decode, options)
     if not success:
         raise result_or_exc
     return result_or_exc
 
 def scp(hostname_or_ip, src, dest, check=True, suppress_fingerprint_warnings=True, local_dest=False):
-    options = ""
+    options = '-o "BatchMode yes"'
     if suppress_fingerprint_warnings:
         # Suppress warnings and questions related to host key fingerprints
         # because on a test network IPs get reused, VMs are reinstalled, etc.

commit ef46e3f67391aa52c89dc30bb17fb063d020bcf3

Fix option accumulation in _ssh()
Commit 0bec9b87a4d6df4c7f6dc9d13a7dd82e67dbf7e9 introduced a regression:
as the default value for parameter `options` is mutable, only one
reference is maintained by python, and so every call to _ssh() adds the
same parameters to the list, again and again, until the following
exception is raised: "OSError: [Errno 7] Argument list too long"

Use a copy of the parameter in the function to fix this.

For variable names consistency, the `options` variable in scp() is also
renamed to `opts`.

Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

lib/commands.py
@@ -66,14 +66,15 @@ OUTPUT_HANDLER.setFormatter(logging.Formatter('%(message)s'))
 
 def _ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprint_warnings=True,
          background=False, target_os='linux', decode=True, options=[]):
-    options.append('-o "BatchMode yes"')
+    opts = list(options)
+    opts.append('-o "BatchMode yes"')
     if suppress_fingerprint_warnings:
         # Suppress warnings and questions related to host key fingerprints
         # because on a test network IPs get reused, VMs are reinstalled, etc.
         # Based on https://unix.stackexchange.com/a/365976/257493
-        options.append('-o "StrictHostKeyChecking no"')
-        options.append('-o "LogLevel ERROR"')
-        options.append('-o "UserKnownHostsFile /dev/null"')
+        opts.append('-o "StrictHostKeyChecking no"')
+        opts.append('-o "LogLevel ERROR"')
+        opts.append('-o "UserKnownHostsFile /dev/null"')
 
     command = " ".join(cmd)
     if background and target_os != "windows":
@@ -82,13 +83,13 @@ def _ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerpri
         # Bash being available on VMs is a documented requirement.
         command = "nohup bash -c \"%s &>/dev/null &\"" % command
 
-    ssh_cmd = f"ssh root@{hostname_or_ip} {' '.join(options)} {shlex.quote(command)}"
+    ssh_cmd = f"ssh root@{hostname_or_ip} {' '.join(opts)} {shlex.quote(command)}"
 
     windows_background = background and target_os == "windows"
     # Fetch banner and remove it to avoid stdout/stderr pollution.
     if config.ignore_ssh_banner and not windows_background:
         banner_res = subprocess.run(
-            "ssh root@%s %s '%s'" % (hostname_or_ip, ' '.join(options), '\n'),
+            "ssh root@%s %s '%s'" % (hostname_or_ip, ' '.join(opts), '\n'),
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
@@ -149,19 +150,19 @@ def ssh(hostname_or_ip, cmd, check=True, simple_output=True, suppress_fingerprin
     return result_or_exc
 
 def scp(hostname_or_ip, src, dest, check=True, suppress_fingerprint_warnings=True, local_dest=False):
-    options = '-o "BatchMode yes"'
+    opts = '-o "BatchMode yes"'
     if suppress_fingerprint_warnings:
         # Suppress warnings and questions related to host key fingerprints
         # because on a test network IPs get reused, VMs are reinstalled, etc.
         # Based on https://unix.stackexchange.com/a/365976/257493
-        options = '-o "StrictHostKeyChecking no" -o "LogLevel ERROR" -o "UserKnownHostsFile /dev/null"'
+        opts = '-o "StrictHostKeyChecking no" -o "LogLevel ERROR" -o "UserKnownHostsFile /dev/null"'
 
     if local_dest:
         src = 'root@{}:{}'.format(hostname_or_ip, src)
     else:
         dest = 'root@{}:{}'.format(hostname_or_ip, dest)
 
-    command = "scp {} {} {}".format(options, src, dest)
+    command = "scp {} {} {}".format(opts, src, dest)
     res = subprocess.run(
         command,
         shell=True,