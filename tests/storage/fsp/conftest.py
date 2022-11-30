0f88e2a3d13de16e7bbac69e0c38527bc5598ed8
Ronan Abhamon <ronan.abhamon@vates.fr>
2022-11-30 16:08:11 +0100

    Repair fsp tests: Install xcp-ng-xapi-storage
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- tests/storage/fsp/conftest.py
+++ tests/storage/fsp/conftest.py
@@ -1,24 +1,43 @@
 import pytest
 
+FSP_REPO_NAME = 'runx'
+
+FSP_PACKAGES = ['xcp-ng-xapi-storage']
+
 DIRECTORIES_PATH = 'directories'
 
+def install_fsp(host):
+    host.add_xcpng_repo(FSP_REPO_NAME)
+    host.yum_save_state()
+    host.yum_install(FSP_PACKAGES)
+
+def uninstall_fsp(host):
+    host.yum_restore_saved_state()
+    host.remove_xcpng_repo(FSP_REPO_NAME)
+
 @pytest.fixture(scope='package')
-def fsp_config(host):
-    db_path = host.ssh(['mktemp', '-d'])
-    shared_dir_path = host.ssh(['mktemp', '-d'])
+def host_with_fsp(host):
+    install_fsp(host)
+    yield host
+    uninstall_fsp(host)
+
+@pytest.fixture(scope='package')
+def fsp_config(host_with_fsp):
+    db_path = host_with_fsp.ssh(['mktemp', '-d'])
+    shared_dir_path = host_with_fsp.ssh(['mktemp', '-d'])
     return {
         'db_path': db_path,
         'shared_dir_path': shared_dir_path
     }
 
 @pytest.fixture(scope='package')
-def fsp_sr(host, fsp_config):
+def fsp_sr(host_with_fsp, fsp_config):
     """ An FSP SR on first host. """
     db_path = fsp_config['db_path']
-    host.ssh(['mkdir', db_path + '/' + DIRECTORIES_PATH])
-    sr = host.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
+    host_with_fsp.ssh(['mkdir', db_path + '/' + DIRECTORIES_PATH])
+    sr = host_with_fsp.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
     yield sr
     # teardown
     sr.destroy()
-    host.ssh(['rm', '-rf', fsp_config['db_path']])
-    host.ssh(['rm', '-rf', fsp_config['shared_dir_path']])
+    host_with_fsp.ssh(['rm', '-rf', fsp_config['db_path']])
+    host_with_fsp.ssh(['rm', '-rf', fsp_config['shared_dir_path']])
--- tests/storage/fsp/test_fsp_sr.py
+++ tests/storage/fsp/test_fsp_sr.py
@@ -6,47 +6,45 @@ from .conftest import DIRECTORIES_PATH
 
 # Requirements:
 # From --hosts parameter:
-# - host: a XCP-ng host >= 8.3
+# - host: a XCP-ng host >= 8.2
 
-@pytest.mark.usefixtures("host_at_least_8_3")
 class TestFSPSRCreateDestroy:
     """
     Tests that do not use fixtures that setup the SR
     because they precisely need to test SR creation and destruction.
     """
 
-    def test_create_and_destroy_sr(self, host):
-        db_path = host.ssh(['mktemp', '-d'])
-        host.ssh(['mkdir', db_path + '/' + DIRECTORIES_PATH])
-        sr = host.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
+    def test_create_and_destroy_sr(self, host_with_fsp):
+        db_path = host_with_fsp.ssh(['mktemp', '-d'])
+        host_with_fsp.ssh(['mkdir', db_path + '/' + DIRECTORIES_PATH])
+        sr = host_with_fsp.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
         sr.destroy()
-        host.ssh(['rm', '-rf', db_path])
+        host_with_fsp.ssh(['rm', '-rf', db_path])
 
-    def test_create_and_destroy_sr_non_existing_path(self, host):
+    def test_create_and_destroy_sr_non_existing_path(self, host_with_fsp):
         # get an unique non-existing path
-        db_path = host.ssh(['mktemp', '-d', '--dry-run'])
+        db_path = host_with_fsp.ssh(['mktemp', '-d', '--dry-run'])
         sr = None
         try:
-            sr = host.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
+            sr = host_with_fsp.sr_create('fsp', "fsp-local-SR-test", {'file-uri': db_path})
         except Exception:
             logging.info("SR creation failed, as expected.")
         if sr is not None:
             sr.destroy()
             assert False, "SR creation should not have succeeded!"
 
-@pytest.mark.usefixtures("host_at_least_8_3")
 class TestFSPVDI:
-    def test_create_and_destroy_VDI(self, host, fsp_sr, fsp_config):
+    def test_create_and_destroy_VDI(self, host_with_fsp, fsp_sr, fsp_config):
         linkname = 'vdifor' + os.path.basename(fsp_config['shared_dir_path'])
         source = fsp_config['shared_dir_path']
         destination = fsp_config['db_path'] + '/' + DIRECTORIES_PATH + '/' + linkname
-        host.ssh(['ln', '-s', source, destination])
+        host_with_fsp.ssh(['ln', '-s', source, destination])
         # scan SR to detect the new VDI
         fsp_sr.scan()
         vdi_uuids = fsp_sr.vdi_uuids(managed=True, name_label=linkname)
         # fail if the vdi could not be created or it already exists
         assert len(vdi_uuids) == 1
-        host.ssh(['rm', '-f', fsp_config['db_path'] + '/' + DIRECTORIES_PATH + '/' + linkname])
+        host_with_fsp.ssh(['rm', '-f', fsp_config['db_path'] + '/' + DIRECTORIES_PATH + '/' + linkname])
         fsp_sr.scan()
         vdi_uuids = fsp_sr.vdi_uuids(managed=True, name_label=linkname)
         # fail if the vdi could not be destroyed
