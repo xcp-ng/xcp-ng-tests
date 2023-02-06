664352e079bcc0eeeca04e6e701f80a3e2d4936d
Samuel Verschelde <stormi-xcp@ylix.fr>
2023-02-06 16:12:16 +0100

    Rename xtf job and directory to xen
    XTF is not the only way to test Xen, and it makes sense to have all
    such tests in the same job and test directory.
    
    So we rename xtf to xen, except the test_xtf.py file itself. This
    prepares the way for adding new tests of the Xen hypervisor itself.
    
    Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

--- jobs.py
+++ jobs.py
@@ -202,14 +202,14 @@ JOBS = {
         "paths": ["tests/guest-tools/unix"],
         "markers": "multi_vms",
     },
-    "xtf": {
-        "description": "Xen's XTF test suite",
+    "xen": {
+        "description": "Testing of the Xen hypervisor itself",
         "requirements": [
             "A host with HVM FEP enabled (`hvm_fep` Xen command line parameter).",
         ],
         "nb_pools": 1,
         "params": {},
-        "paths": ["tests/xtf"],
+        "paths": ["tests/xen"],
     },
     "flaky": {
         "description": "tests that usually pass, but sometimes fail unexpectedly",
