b66c1c630e3cea5bf4c9ab0827e1479da8389d54
Benjamin Reis <benjamin.reis@vates.tech>
2024-01-19 14:59:12 +0100

    Assemble markers from job and cli
    Signed-off-by: Benjamin Reis <benjamin.reis@vates.tech>

--- jobs.py
+++ jobs.py
@@ -288,23 +288,29 @@ def build_pytest_cmd(job_data, hosts=None, pytest_args=[]):
 
     job_params = dict(job_data["params"])
 
-    # Merge name filter
-    cli_name_filters = []
-    try:
-        while True:
-            i = pytest_args.index("-k")
-            value = pytest_args[i + 1]
-            del pytest_args[i + 1]
-            del pytest_args[i]
-            cli_name_filters.append(value)
-    except ValueError:
-        pass
+    def _join_pytest_args(arg, option):
+        cli_args = []
+        try:
+            while True:
+                i = pytest_args.index(option)
+                value = pytest_args[i + 1]
+                del pytest_args[i + 1]
+                del pytest_args[i]
+                cli_args.append(value)
+        except ValueError:
+            pass
+        joined_cli_args = ") and (".join(cli_args)
+        if arg and joined_cli_args:
+            return f"({arg}) and ({joined_cli_args})"
+        if joined_cli_args:
+            return f"({joined_cli_args})"
+        return arg
 
-    joined_cli_filters = ") and (".join(cli_name_filters)
-    if name_filter and joined_cli_filters:
-        name_filter = f"({name_filter}) and ({joined_cli_filters})"
-    elif joined_cli_filters:
-        name_filter = f"({joined_cli_filters})"
+    # Merge name filter
+    name_filter = _join_pytest_args(name_filter, "-k")
+
+    # Merge markers
+    markers = _join_pytest_args(markers, "-m")
 
     # pytest_args may override job_params
     pytest_args_keys = []
