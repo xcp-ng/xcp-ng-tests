1b8ba95276ff51021aeb633b321b7b61e044fbb0
Benjamin Reis <benjamin.reis@vates.tech>
2024-01-23 10:30:29 +0100

    Add a test for connection XO to hosts
    Signed-off-by: Benjamin Reis <benjamin.reis@vates.tech>

---
+++ tests/misc/test_xo_connection.py
@@ -0,0 +1,12 @@
+import logging
+
+# This test is meant to test an XO connection.
+#
+# Requirements:
+# - an XCP-ng host >= 8.2 with latest updates.
+# - An XOA registered through `xo-cli`
+
+def test_xo_connection(hosts_with_xo):
+    for h in hosts_with_xo:
+        logging.debug(f"Testing connection for host: {h.hostname_or_ip}")
+        assert h.xo_get_server_id() is not None
