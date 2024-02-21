c0d740c39ba971138c2727f23e94146e7d5d61f0
Samuel Verschelde <stormi-xcp@ylix.fr>
2024-02-21 16:10:08 +0100

    Get the list of dom0 tests from the metadata file
    Signed-off-by: Samuel Verschelde <stormi-xcp@ylix.fr>

--- tests/xen/test_xen_dom0_tests.py
+++ tests/xen/test_xen_dom0_tests.py
@@ -1,4 +1,5 @@
 import logging
+import json
 import pytest
 
 # Requirements:
@@ -7,10 +8,10 @@ import pytest
 
 @pytest.mark.usefixtures("host_with_dom0_tests")
 class TestXenDom0Tests:
-    def test_cpu_policy(self, host):
-        logging.info("Running test-cpu-policy...")
-        host.ssh(['/usr/libexec/xen/bin/test-cpu-policy'])
-
-    def test_xenstore(self, host):
-        logging.info("Running test-xenstore...")
-        host.ssh(['/usr/libexec/xen/bin/test-xenstore'])
+    def test_dom0(self, host):
+        metadata = host.ssh(['cat', '/usr/share/xen-dom0-tests-metadata.json'])
+        tests = json.loads(metadata)["tests"]
+        assert tests, "Test list must not be empty"
+        for test in tests:
+            logging.info(f"Running {test}...")
+            host.ssh([f'/usr/libexec/xen/bin/{test}'])
