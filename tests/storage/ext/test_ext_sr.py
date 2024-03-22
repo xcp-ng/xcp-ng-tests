761520f3e61c4019b67f26fbb69990161610e263
Yann Dirson <yann.dirson@vates.tech>
2024-03-22 16:19:29 +0100

    test_*_sr: fix test_snapshot (x)fail not to contaminate subsequent tests
    All SR's test_snapshot take a stopped VM from vm_on_*_sr fixture.  Then
    they start it, and hopefully stop it if everything went well.
    
    Now if something went bad the shutdown would never be requested.  If
    other tests are then run after a failure (which currently requires using
    `--maxfail` to overrule the config file), or if such a test is marked
    xfail (like the first public prototype for zfs-ng), subsequent tests chained
    by pytest would be launched with a VM wrongly started, and fail for this
    reason, which is bad.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- tests/storage/ext/test_ext_sr.py
+++ tests/storage/ext/test_ext_sr.py
@@ -46,9 +46,11 @@ class TestEXTSR:
     def test_snapshot(self, vm_on_ext_sr):
         vm = vm_on_ext_sr
         vm.start()
-        vm.wait_for_os_booted()
-        vm.test_snapshot_on_running_vm()
-        vm.shutdown(verify=True)
+        try:
+            vm.wait_for_os_booted()
+            vm.test_snapshot_on_running_vm()
+        finally:
+            vm.shutdown(verify=True)
 
     # *** tests with reboots (longer tests).
 
