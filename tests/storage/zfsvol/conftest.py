f48e1024d228a64f69a2b1431041155b6bc6e181
Yann Dirson <yann.dirson@vates.tech>
2024-03-27 14:35:18 +0100

    Add tests for zfs-vol
    Those are derived from the zfs tests.
    
    Some tests were not ported, the expectation being we'll ship a
    dedicated RPM for the SR driver, which will pull the zfs package,
    which removes a whole bunch of trick situations.  We may want to write
    tests for the case where the SR driver is not installed, but they will
    be simpler.
    
    Not all quicktests pass yet.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

---
+++ tests/storage/zfsvol/conftest.py
@@ -0,0 +1,35 @@
+import logging
+import pytest
+
+# Explicitly import package-scoped fixtures (see explanation in pkgfixtures.py)
+from pkgfixtures import host_with_saved_yum_state, sr_disk_wiped
+
+@pytest.fixture(scope='package')
+def host_with_zfsvol(host_with_saved_yum_state):
+    host = host_with_saved_yum_state
+    host.yum_install(['xcp-ng-xapi-storage-volume-zfsvol'])
+    yield host
+
+@pytest.fixture(scope='package')
+def zfsvol_sr(host, sr_disk_wiped, host_with_zfsvol):
+    """ A ZFS Volume SR on first host. """
+    device = '/dev/' + sr_disk_wiped
+    sr = host.sr_create('zfs-vol', "ZFS-local-SR-test", {'device': device})
+    yield sr
+    # teardown violently - we don't want to require manual recovery when a test fails
+    sr.forget()
+    host.ssh(["wipefs", "-a", device])
+
+@pytest.fixture(scope='module')
+def vdi_on_zfsvol_sr(zfsvol_sr):
+    vdi = zfsvol_sr.create_vdi('ZFS-local-VDI-test')
+    yield vdi
+    vdi.destroy()
+
+@pytest.fixture(scope='module')
+def vm_on_zfsvol_sr(host, zfsvol_sr, vm_ref):
+    vm = host.import_vm(vm_ref, sr_uuid=zfsvol_sr.uuid)
+    yield vm
+    # teardown
+    logging.info("<< Destroy VM")
+    vm.destroy(verify=True)
