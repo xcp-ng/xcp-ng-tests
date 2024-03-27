6b71c78d6acb5efe98e7fdd935f8436f3ada8b0a
Yann Dirson <yann.dirson@vates.tech>
2024-03-27 14:35:40 +0100

    lib/vdi: add shapshot and clone operations
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- lib/vdi.py
+++ lib/vdi.py
@@ -1,10 +1,22 @@
 import logging
 
 class VDI:
-    def __init__(self, sr, uuid):
+    def __init__(self, sr, uuid, snapshot_of=None):
         self.uuid = uuid
+        self.snapshot_of = snapshot_of
         # TODO: use a different approach when migration is possible
         self.sr = sr
+
+    def snapshot(self):
+        logging.info(f"Create snapshot of {self}")
+        return VDI(self.sr,
+                   self.sr.pool.master.xe('vdi-snapshot', {'uuid': self.uuid}),
+                   snapshot_of=self)
+
+    def clone(self):
+        logging.info(f"Create clone of {self}")
+        return VDI(self.sr,
+                   self.sr.pool.master.xe('vdi-clone', {'uuid': self.uuid}))
 
     def destroy(self):
         logging.info(f"Destroy {self}")
