3c93e2e2152baa2fcd0d977711947abd4f28b360
Yann Dirson <yann.dirson@vates.tech>
2024-03-27 15:34:59 +0100

    zfs-vol: add (xfail) migration tests
    Those are copied from the zfs tests.  Cannot pass yet since there is
    no migration support in there.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

---
+++ tests/storage/zfsvol/test_zfsvol_sr_intrapool_migration.py
@@ -0,0 +1,26 @@
+import pytest
+from tests.storage import cold_migration_then_come_back, live_storage_migration_then_come_back
+
+# Requirements:
+# From --hosts parameter:
+# - host(A1): first XCP-ng host >= 8.2 with an additional unused disk for the SR.
+# - hostA2: Second member of the pool. Can have any local SR. No need to specify it on CLI.
+# From --vm parameter
+# - A VM to import to the ZFS-ng SR
+# And:
+# - access to XCP-ng RPM repository from hostA1
+
+@pytest.mark.xfail
+@pytest.mark.small_vm # run with a small VM to test the features
+@pytest.mark.big_vm # and ideally with a big VM to test it scales
+@pytest.mark.usefixtures("hostA2", "local_sr_on_hostA2")
+class Test:
+    def test_cold_intrapool_migration(self, host, hostA2, vm_on_zfsvol_sr,
+                                      zfsvol_sr, local_sr_on_hostA2):
+        cold_migration_then_come_back(
+            vm_on_zfsvol_sr, host, zfsvol_sr, hostA2, local_sr_on_hostA2)
+
+    def test_live_intrapool_migration(self, host, hostA2, vm_on_zfsvol_sr,
+                                      zfsvol_sr, local_sr_on_hostA2):
+        live_storage_migration_then_come_back(
+            vm_on_zfsvol_sr, host, zfsvol_sr, hostA2, local_sr_on_hostA2)
