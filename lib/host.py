78a86986ceaa30d3551c51c3aa6b3fa52c8c9301
Yann Dirson <yann.dirson@vates.tech>
2024-04-02 18:50:39 +0200

    Host.reboot: use intermediate steps to better understand reboot delays
    Code as showing "Wait for host up" while indeed waiting for XAPI to be up
    and running.  This splits it between "Wait for host up" (== answers to
    ping), "Wait for ssh up on host" (== port 22 open), and "Wait for XAPI to
    be ready" (xapi says so over known-working ssh).
    
    This notably allows to see some 1-minute delays between working IP and
    open port.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- lib/host.py
+++ lib/host.py
@@ -383,7 +383,11 @@ class Host:
                 raise
         if verify:
             wait_for_not(self.is_enabled, "Wait for host down")
-            wait_for(self.is_enabled, "Wait for host up", timeout_secs=30 * 60)
+            wait_for(lambda: not os.system(f"ping -c1 {self.hostname_or_ip} > /dev/null 2>&1"),
+                     "Wait for host up", retry_delay_secs=10)
+            wait_for(lambda: not os.system(f"nc -zw5 {self.hostname_or_ip} 22"),
+                     "Wait for ssh up on host", retry_delay_secs=5)
+            wait_for(self.is_enabled, "Wait for XAPI to be ready", timeout_secs=30 * 60)
 
     def management_network(self):
         return self.xe('network-list', {'bridge': self.inventory['MANAGEMENT_INTERFACE']}, minimal=True)
