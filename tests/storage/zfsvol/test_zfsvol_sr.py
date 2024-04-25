b1bc1f8c8da3132bef15bb348c79f4a039e7ee4d
Yann Dirson <yann.dirson@vates.tech>
2024-04-25 14:48:23 +0200

    WIP test_create_and_destroy_sr: make sure SR is destroyed if VM.create fails
    TODO: all other SR

--- tests/storage/zfsvol/test_zfsvol_sr.py
+++ tests/storage/zfsvol/test_zfsvol_sr.py
@@ -20,13 +20,18 @@ class TestZfsvolSRCreateDestroy:
     """
 
     def test_create_and_destroy_sr(self, host, sr_disk_wiped):
-        # Create and destroy tested in the same test to leave the host as unchanged as possible
-        sr = host.sr_create('zfs-vol', "ZFS-local-SR-test", {'device': '/dev/' + sr_disk_wiped}, verify=True)
-        # import a VM in order to detect vm import issues here rather than in the vm_on_xfs_fixture used in
-        # the next tests, because errors in fixtures break teardown
-        vm = host.import_vm(vm_image('mini-linux-x86_64-bios'), sr_uuid=sr.uuid)
-        vm.destroy(verify=True)
-        sr.destroy(verify=True)
+        # Create and destroy tested in the same test to leave the host
+        # as unchanged as possible
+        sr = host.sr_create('zfs-vol', "ZFS-local-SR-test",
+                            {'device': '/dev/' + sr_disk_wiped}, verify=True)
+        try:
+            # import a VM in order to detect vm import issues here
+            # rather than in the vm_on_xfs_fixture used in the next
+            # tests, because errors in fixtures break teardown
+            vm = host.import_vm(vm_image('mini-linux-x86_64-bios'), sr_uuid=sr.uuid)
+            vm.destroy(verify=True)
+        finally:
+            sr.destroy(verify=True)
 
 @pytest.mark.usefixtures("zfsvol_sr")
 class TestZfsvolSrBasics:
