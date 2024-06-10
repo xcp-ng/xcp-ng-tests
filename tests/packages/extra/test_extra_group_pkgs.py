d5952b3584d117718946d154ee782aa42e622723
Yann Dirson <yann.dirson@vates.tech>
2024-06-10 16:38:02 +0200

    test_extra_group_pkgs: fix pkgfixture usage
    pkgfixture stuff must *always* be used from conftest.py, or else
    strange problems appear because of the result shown in
    https://github.com/pytest-dev/pytest/issues/8189#issuecomment-2158537878
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- tests/packages/extra/test_extra_group_pkgs.py
+++ tests/packages/extra/test_extra_group_pkgs.py
@@ -1,10 +1,3 @@
-# Explicitly import package-scoped fixtures (see explanation in pkgfixtures.py)
-from pkgfixtures import host_with_saved_yum_state
-
-# Requirements:
-# From --hosts parameter:
-# - host(A1): any master host of a pool, with access to XCP-ng RPM repositories and reports.xcp-ng.org.
-
 def test_extra_group_packages_url_resolved(host, extra_pkgs):
     for p in extra_pkgs:
         host.ssh(['yumdownloader', '--resolve', '--urls', p])
