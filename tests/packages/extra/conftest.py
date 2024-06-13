d4923e3552f68524f56e2ecf8aaf4e4de2e03318
Yann Dirson <yann.dirson@vates.tech>
2024-06-13 14:54:57 +0200

    test_extra_group_pkgs: restore Requirements into proper location
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- tests/packages/extra/conftest.py
+++ tests/packages/extra/conftest.py
@@ -4,9 +4,6 @@ import urllib.request
 
 # Explicitly import package-scoped fixtures (see explanation in pkgfixtures.py)
 from pkgfixtures import host_with_saved_yum_state
-# Requirements:
-# From --hosts parameter:
-# - host(A1): any master host of a pool, with access to XCP-ng RPM repositories and reports.xcp-ng.org.
 
 @pytest.fixture(scope="session")
 def extra_pkgs(host):
