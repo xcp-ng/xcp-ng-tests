120c4fe9352ff9b9893e857d2defa1536c3922c8
Yann Dirson <yann.dirson@vates.tech>
2024-06-26 12:17:02 +0200

    WIP install 7/n: add firstboot test
    FIXME
    - Pool construction on firstboot is problematic (causing flaky test)

--- tests/install/test_nested.py
+++ tests/install/test_nested.py
@@ -108,3 +108,136 @@ def test_install(iso_remaster, create_vms):
         # wait_for(lambda: False, 'Wait "forever"', timeout_secs=100*60)
         host_vm.shutdown(force=True)
         raise
+
+@pytest.mark.dependency(depends=["test_install"])
+@pytest.mark.vm_definitions(
+    dict(name="vm1", image_test="test_install"))
+class TestFirstboot:
+    @pytest.fixture(autouse=True, scope="class")
+    def firstboot_host(self, create_vms):
+        host_vm = create_vms[0]
+        vif = host_vm.vifs()[0]
+        mac_address = vif.param_get('MAC')
+        logging.info("Host VM has MAC %s", mac_address)
+
+        try:
+            # FIXME: evict MAC from ARP cache first?
+            host_vm.start()
+            wait_for(host_vm.is_running, "Wait for host VM running")
+
+            # catch host-vm IP address
+            wait_for(lambda: pxe.arp_addresses_for(mac_address),
+                     "Wait for DHCP server to see Host VM in ARP tables",
+                     timeout_secs=10*60)
+            ips = pxe.arp_addresses_for(mac_address)
+            logging.info("Host VM has IPs %s", ips)
+            assert len(ips) == 1
+            host_vm.ip = ips[0]
+
+            wait_for(lambda: not os.system(f"nc -zw5 {host_vm.ip} 22"),
+                     "Wait for ssh back up on Host VM", retry_delay_secs=5)
+
+            # FIXME "xe host-list" in there can fail with various
+            # errors until the XAPI DB is initialized enough for
+            # Pool.__init__(), which assumes the master XAPI is up and
+            # running.
+
+            # pool master must be reachable here
+            # FIXME: not sure why we seem to need this, while port 22 has been seen open
+            tries = 5
+            while True:
+                try:
+                    pool = Pool(host_vm.ip)
+                except commands.SSHCommandFailed as e:
+                    if "Connection refused" not in e.stdout:
+                        raise
+                    tries -= 1
+                    if tries:
+                        logging.warning("retrying connection to pool master")
+                        time.sleep(2)
+                        continue
+                    # retries failed
+                    raise
+                # it worked!
+                break
+
+            # wait for XAPI
+            wait_for(pool.master.is_enabled, "Wait for XAPI to be ready", timeout_secs=30 * 60)
+
+            yield pool.master
+
+            logging.info("Powering off pool master")
+            try:
+                # use "poweroff" because "reboot" would cause ARP and
+                # SSH to be checked before host is down, and require
+                # ssh retries
+                pool.master.ssh(["poweroff"])
+            except commands.SSHCommandFailed as e:
+                # ignore connection closed by reboot
+                if e.returncode == 255 and "closed by remote host" in e.stdout:
+                    logging.info("sshd closed the connection")
+                    pass
+                else:
+                    raise
+
+            wait_for(host_vm.is_halted, "Wait for host VM halted")
+
+        except Exception as e:
+            logging.critical("caught exception %s", e)
+            # wait_for(lambda: False, 'Wait "forever"', timeout_secs=100*60)
+            host_vm.shutdown(force=True)
+            raise
+        except KeyboardInterrupt:
+            logging.warning("keyboard interrupt")
+            # wait_for(lambda: False, 'Wait "forever"', timeout_secs=100*60)
+            host_vm.shutdown(force=True)
+            raise
+
+    # WARNING: tests here should not have any side-effect: then can be
+    # run in any order, and an output cache VM will be produced even
+    # if only one of the tests gets run (as long as it passes).
+
+    def test_firstboot_services(self, firstboot_host):
+        host = firstboot_host
+
+        # check for firstboot issues
+        # FIXME: flaky, must check logs extraction on failure
+        for service in ["control-domain-params-init",
+                        "network-init",
+                        "storage-init",
+                        "generate-iscsi-iqn",
+                        "create-guest-templates",
+                        ]:
+            try:
+                wait_for(lambda: host.ssh(["test", "-e", f"/var/lib/misc/ran-{service}"],
+                                          check=False, simple_output=False,
+                                          ).returncode == 0,
+                         f"Wait for ran-{service} stamp")
+            except TimeoutError:
+                logging.warning("investigating lack of ran-%s stamp", service)
+                out = host.ssh(["systemctl", "status", service], check=False)
+                logging.warning("service status: %s", out)
+                out = host.ssh(["grep", "-r", service, "/var/log"], check=False)
+                logging.warning("in logs: %s", out)
+                raise
+
+    def test_product_version(self, firstboot_host, mode):
+        host = firstboot_host
+
+        # determine version info from `mode`
+        expected_dist = "XCP-ng"
+        # succession of insta/upg/rst operations
+        split_mode = mode.split("-")
+        if len(split_mode) == 3:
+            # restore: back to 1st installed version
+            expected_rel_id = split_mode[0]
+        else:
+            expected_rel_id = split_mode[-1]
+        expected_rel = {
+            "821.1": "8.2.1",
+            "83b2": "8.3.0",
+        }[expected_rel_id]
+
+        lsb_dist = host.ssh(["lsb_release", "-si"])
+        lsb_rel = host.ssh(["lsb_release", "-sr"])
+        assert (lsb_dist, lsb_rel) == (expected_dist, expected_rel)
