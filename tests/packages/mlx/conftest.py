981698a9a1a84204a46787866d8c97436ccb79d4
Guillaume <guillaume.thouvenin@vates.tech>
2024-06-26 10:30:55 +0200

    Add a test to check compatibility between mellanox drivers
    The mlx4 driver requires mlx_compat.ko from the mlx5 driver and the
    version differ. Someday compatibility may break. This test will detect
    it.
    
    Signed-off-by: Guillaume <guillaume.thouvenin@vates.tech>

---
+++ tests/packages/mlx/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import logging
+
+# Explicitly import package-scoped fixtures (see explanation in pkgfixtures.py)
+from pkgfixtures import host_with_saved_yum_state
+
+@pytest.fixture(scope="package")
+def host_without_mlx_compat_loaded(host_with_saved_yum_state):
+    host = host_with_saved_yum_state
+
+    # We need to check if mlx_compat module is loaded. If it is already loaded,
+    # we need to unload it before starting the test and load it again when the test ends.
+    mlx_compat_loaded = host.ssh_with_result(['lsmod', '|', 'grep', 'mlx_compat']).returncode == 0
+    if mlx_compat_loaded:
+        logging.info("mlx_compat is loaded so unload it before testing")
+        host.ssh(['modprobe', '-r', '-v', 'mlx_compat'])
+    else:
+        logging.info("mlx_compat is not loaded")
+
+    yield host
+
+    # Reload the mlx_compat module if it was loaded when starting the test
+    if mlx_compat_loaded:
+        logging.info("test is done so reload mlx_compat")
+        host.ssh(['modprobe', '-v', 'mlx_compat'])
