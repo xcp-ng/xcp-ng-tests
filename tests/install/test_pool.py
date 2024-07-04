2f737e2b87774f43d0110866b3fd9f053c63a241
Yann Dirson <yann.dirson@vates.tech>
2024-07-04 11:59:50 +0200

    New test: create VMs on shared SR in pool

--- tests/install/test_pool.py
+++ tests/install/test_pool.py
@@ -3,9 +3,11 @@ import os
 import pytest
 
 from lib import pxe
-from lib.common import wait_for
+from lib.common import wait_for, vm_image
 from lib.pool import Pool
 
+# FIXME without --ignore-unknown-dependency, SKIPPED
+# "because it depends on tests/install/test.py::TestNested::test_firstboot_install[uefi-821.1-host1]"
 @pytest.mark.usefixtures("xcpng_chained")
 @pytest.mark.parametrize("mode", (
     "821.1",
@@ -21,7 +23,7 @@ from lib.pool import Pool
          scope="package"),
 ],
                              param_mapping={"params": "mode", "firmware": "firmware"})
-def test_join_pool(firmware, mode, create_vms):
+def test_pool_rpu(firmware, mode, create_vms):
     (master_vm, slave_vm) = create_vms
     master_mac = master_vm.vifs()[0].param_get('MAC')
     slave_mac = slave_vm.vifs()[0].param_get('MAC')
@@ -54,11 +56,36 @@ def test_join_pool(firmware, mode, create_vms):
              "Wait for ssh up on Slave VM", retry_delay_secs=5)
 
     pool = Pool(master_vm.ip)
+
+    # create pool with shared SR
+
     slave = Pool(slave_vm.ip).master
     slave.join_pool(pool)
 
+    from data import NFS_DEVICE_CONFIG
+    sr = pool.master.sr_create("nfs", "NFS Shared SR", NFS_DEVICE_CONFIG,
+                               shared=True, verify=True)
+
+    # create and start VMs
+    vms = (
+        pool.master.import_vm(vm_image('mini-linux-x86_64-bios'), sr_uuid=sr.uuid),
+        pool.master.import_vm(vm_image('mini-linux-x86_64-bios'), sr_uuid=sr.uuid),
+    )
+
+    for vm in vms:
+        vm.start()
+
+    wait_for(lambda: all(vm.is_running() for vm in vms), "Wait for VMs running")
+    wait_for(lambda: all(vm.try_get_and_store_ip() for vm in vms),
+             "Wait for VM IP", timeout_secs=5*60)
+    wait_for(lambda: all(vm.is_management_agent_up() for vm in vms),
+             "Wait for management agent up")
+
+    logging.info("VMs dispatched as %s", [vm.get_residence_host().uuid for vm in vms])
+
+    # cleanup
     slave.shutdown()
     pool.master.shutdown()
-
-    wait_for(lambda: slave_vm.is_halted(), "Wait for Slave VM to be halted")
-    wait_for(lambda: master_vm.is_halted(), "Wait for Master VM to be halted")
+    wait_for(lambda: slave_vm.is_halted(), "Wait for Slave VM to be halted", timeout_secs=5*60)
+    wait_for(lambda: master_vm.is_halted(), "Wait for Master VM to be halted", timeout_secs=5*60)
+    # FIXME destroy shared SR contents
