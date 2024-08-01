8c97a17ff5d3dd8a385302b1bb129e5e53d323a7
Yann Dirson <yann.dirson@vates.tech>
2024-08-01 14:22:00 +0200

    VDI: add param_* APIs
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- lib/vdi.py
+++ lib/vdi.py
@@ -1,6 +1,10 @@
 import logging
 
+from lib.common import _param_add, _param_clear, _param_get, _param_remove, _param_set
+
 class VDI:
+    xe_prefix = "vdi"
+
     def __init__(self, sr, uuid):
         self.uuid = uuid
         # TODO: use a different approach when migration is possible
@@ -12,3 +16,23 @@ class VDI:
 
     def __str__(self):
         return f"VDI {self.uuid} on SR {self.sr.uuid}"
+
+    def param_get(self, param_name, key=None, accept_unknown_key=False):
+        return _param_get(self.sr.pool.master, self.xe_prefix, self.uuid,
+                          param_name, key, accept_unknown_key)
+
+    def param_set(self, param_name, value, key=None):
+        _param_set(self.sr.pool.master, self.xe_prefix, self.uuid,
+                   param_name, value, key)
+
+    def param_add(self, param_name, value, key=None):
+        _param_add(self.sr.pool.master, self.xe_prefix, self.uuid,
+                   param_name, value, key)
+
+    def param_clear(self, param_name):
+        _param_clear(self.sr.pool.master, self.xe_prefix, self.uuid,
+                     param_name)
+
+    def param_remove(self, param_name, key, accept_unknown_key=False):
+        _param_remove(self.sr.pool.master, self.xe_prefix, self.uuid,
+                      param_name, key, accept_unknown_key)
