7e220fc685330288ae650aa60ea226de403324c4
Yann Dirson <yann.dirson@vates.tech>
2024-08-09 16:26:00 +0200

    WIP install 4/n: boot and monitor installer
    Currently relies on the installer fetching the answerfile from the PXE
    server, to ensure the latter's ARP tables are populated with the
    obtained DHCP address.
    
    FIXME:
    - specification of answerfile contents and dom0 cmdline is tightly linked,
      e.g. we rely on atexit=shell
    - repeatedly polling with grep is junk

--- tests/install/conftest.py
+++ tests/install/conftest.py
@@ -3,10 +3,11 @@ import os
 import pytest
 import tempfile
 
-from lib.common import callable_marker
+from lib import installer, pxe
+from lib.common import callable_marker, wait_for
 from lib.commands import local_cmd, scp, ssh
 
-from data import ANSWERFILE_URL, ISO_IMAGES, ISOSR_SRV, ISOSR_PATH, TOOLS
+from data import ANSWERFILE_URL, ISO_IMAGES, ISOSR_SRV, ISOSR_PATH, TEST_SSH_PUBKEY, TOOLS
 
 @pytest.fixture(scope='function')
 def installer_iso(request):
@@ -22,16 +23,39 @@ def installer_iso(request):
     return iso
 
 @pytest.fixture(scope='function')
-def iso_remaster(request, installer_iso):
+def remastered_iso(request, installer_iso):
     assert "iso-remaster" in TOOLS
     iso_remaster = TOOLS["iso-remaster"]
     assert os.access(iso_remaster, os.X_OK)
 
     with tempfile.TemporaryDirectory() as isotmp:
         remastered_iso = os.path.join(isotmp, "image.iso")
+        img_patcher_script = os.path.join(isotmp, "img-patcher")
         iso_patcher_script = os.path.join(isotmp, "iso-patcher")
 
         logging.info("Remastering %s to %s", installer_iso, remastered_iso)
+
+        # generate install.img-patcher script
+        with open(img_patcher_script, "xt") as patcher_fd:
+            print(f"""#!/bin/bash
+set -ex
+INSTALLIMG="$1"
+
+mkdir -p "$INSTALLIMG/root/.ssh"
+echo "{TEST_SSH_PUBKEY}" > "$INSTALLIMG/root/.ssh/authorized_keys"
+
+cat > "$INSTALLIMG/root/postinstall.sh" <<EOF
+#!/bin/sh
+set -ex
+
+ROOT="\\$1"
+
+mkdir -p "\\$ROOT/root/.ssh"
+echo "{TEST_SSH_PUBKEY}" >> "\\$ROOT/root/.ssh/authorized_keys"
+EOF
+""",
+                  file=patcher_fd)
+            os.chmod(patcher_fd.fileno(), 0o755)
 
         # generate iso-patcher script
         with open(iso_patcher_script, "xt") as patcher_fd:
@@ -51,6 +75,7 @@ sed -i "${{SED_COMMANDS[@]}}" \
 
         # do remaster
         local_cmd([iso_remaster,
+                   "--install-patcher", img_patcher_script,
                    "--iso-patcher", iso_patcher_script,
                    installer_iso, remastered_iso
                    ])
@@ -58,10 +83,13 @@ sed -i "${{SED_COMMANDS[@]}}" \
         yield remastered_iso
 
 @pytest.fixture(scope='function')
-def vm_booted_with_installer(installer_iso, create_vms):
+def vm_booted_with_installer(remastered_iso, create_vms):
     host_vm, = create_vms # one single VM
-    iso = installer_iso
+    vif = host_vm.vifs()[0]
+    mac_address = vif.param_get('MAC')
+    logging.info("Host VM has MAC %s", mac_address)
 
+    iso = remastered_iso
     # unique filename on server, has to work on FreeBSD-based NAS
     # too, and even v14 has no tool allowing mktemp suffixes
     remote_iso = ssh(ISOSR_SRV,
@@ -75,9 +103,36 @@ def vm_booted_with_installer(installer_iso, create_vms):
     try:
         scp(ISOSR_SRV, iso, remote_iso)
         # FIXME: run sr-scan
-
         host_vm.insert_cd(os.path.basename(remote_iso))
-        yield host_vm
+
+        try:
+            host_vm.start()
+            wait_for(host_vm.is_running, "Wait for host VM running")
+
+            # catch host-vm IP address
+            wait_for(lambda: pxe.arp_addresses_for(mac_address),
+                     "Wait for DHCP server to see Host VM in ARP tables",
+                     timeout_secs=10*60)
+            ips = pxe.arp_addresses_for(mac_address)
+            logging.info("Host VM has IPs %s", ips)
+            assert len(ips) == 1
+            host_vm.ip = ips[0]
+
+            yield host_vm
+
+            logging.info("Shutting down Host VM after successful installation")
+            installer.poweroff(host_vm.ip)
+            wait_for(host_vm.is_halted, "Wait for host VM halted")
+
+        except Exception as e:
+            logging.critical("caught exception %s", e)
+            host_vm.shutdown(force=True)
+            raise
+        except KeyboardInterrupt:
+            logging.warning("keyboard interrupt")
+            host_vm.shutdown(force=True)
+            raise
+
         host_vm.eject_cd()
     finally:
         logging.info("Removing %s from ISO-SR server", os.path.basename(remote_iso))
