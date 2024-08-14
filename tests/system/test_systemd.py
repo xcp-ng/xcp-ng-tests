b9a95729ff58e83ec8774039f5032eddefdb27e0
Thierry Escande <thierry.escande@vates.tech>
2024-08-14 14:40:20 +0200

    Add test for systemd default target
    This test uses 'systemd-analyze verify default.target' and check for
    unknown issues. Known issues reported by systemd-analyze are listed in a
    white list array. Anything else is reported as error.
    
    Signed-off-by: Thierry Escande <thierry.escande@vates.tech>

--- tests/system/test_systemd.py
+++ tests/system/test_systemd.py
@@ -13,3 +13,19 @@ def test_failed_units(host):
         logging.error(f"Unit {unit.split()[0]} failed")
 
     assert not failed_services
+
+white_list_issues = [
+    "Cannot add dependency job for unit getty@tty1.service, ignoring: Unit is masked.",
+    "Cannot add dependency job for unit display-manager.service, ignoring: Unit not found.",
+]
+
+pytest.fixture(scope='module')
+def test_verify_default_target(host):
+    analyse = host.ssh(['systemd-analyze', 'verify', 'default.target'])
+    err = False
+    for line in analyse.splitlines():
+        if line not in white_list_issues:
+            logging.error(f"{line}")
+            err = True
+
+    assert not err
