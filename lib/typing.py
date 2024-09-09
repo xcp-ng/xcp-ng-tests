ba67db4f3bc18ad46fb658927038e5eed1df2f69
Guillaume <guillaume.thouvenin@vates.tech>
2024-09-09 19:11:40 +0200

    Add support for netinstall
    Add a new parameter to allow booting using netinstall. If an ISO only
    supports netinstall, it is specified it in the data.py by setting the
    `net-only`option to True. This option is set to False by default.
    
    Signed-off-by: Guillaume <guillaume.thouvenin@vates.tech>
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- lib/typing.py
+++ lib/typing.py
@@ -3,5 +3,7 @@ from typing_extensions import NotRequired
 
 IsoImageDef = TypedDict('IsoImageDef',
                         {'path': str,
+                         'net-url': NotRequired[str],
+                         'net-only': NotRequired[bool],
                          'unsigned': NotRequired[bool],
                          })
