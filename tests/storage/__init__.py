5c432c891740a3fc565ad155c38ebe15e2f9b4e8
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2025-04-14 12:04:42 +0200

    fix F403 undefined-local-with-import-star
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/__init__.py
+++ tests/storage/__init__.py
@@ -1,1 +1,6 @@
-from .storage import *
+from .storage import (
+    cold_migration_then_come_back,
+    live_storage_migration_then_come_back,
+    try_to_create_sr_with_missing_device,
+    vdi_is_open,
+)
