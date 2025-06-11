4c3c0b01d5928334c392cb410280a733bf6f7ad4
Anthoine Bourgeois <anthoine.bourgeois@vates.tech>
2025-06-11 17:30:52 +0200

    test_systemd: whitelist polkit dependency message
    This message only append with xostor activated.
    drbd-reactor.service depends on polkit service.
    
    Signed-off-by: Anthoine Bourgeois <anthoine.bourgeois@vates.tech>

--- tests/system/test_systemd.py
+++ tests/system/test_systemd.py
@@ -17,6 +17,7 @@ white_list_issues = [
     "Cannot add dependency job for unit display-manager.service, ignoring: Unit not found.",
     "Cannot add dependency job for unit qemuback.service, ignoring: Unit not found.",
     "Cannot add dependency job for unit sr_health_check.timer, ignoring: Unit not found.",
+    "Cannot add dependency job for unit polkit.service, ignoring: Unit not found.",
 ]
 
 pytest.fixture(scope='module')
