c424cca02fe96a0920d036b19dcd85673132ccb9
Thierry Escande <thierry.escande@vates.tech>
2025-06-16 15:54:08 +0200

    host_with_perf: Set to 8.3 hosts only
    Make perf_probe test to run on 8.3 hosts only.
    
    Signed-off-by: Thierry Escande <thierry.escande@vates.tech>

--- tests/packages/linux_image/conftest.py
+++ tests/packages/linux_image/conftest.py
@@ -6,7 +6,7 @@ import urllib.request
 from pkgfixtures import host_with_saved_yum_state
 
 @pytest.fixture(scope="package")
-def host_with_perf(host_with_saved_yum_state):
+def host_with_perf(host_at_least_8_3, host_with_saved_yum_state):
     host = host_with_saved_yum_state
 
     logging.info(f"Getting perf package")
