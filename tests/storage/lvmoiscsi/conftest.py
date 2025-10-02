61f075d82d769b5298a1c6f09981a3c369b2c947
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2025-10-02 11:08:26 +0200

    lvmoiscsi: use 1GiB disk size by default
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/lvmoiscsi/conftest.py
+++ tests/storage/lvmoiscsi/conftest.py
@@ -4,6 +4,11 @@ import logging
 
 from lib import config
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lib.sr import SR
+
 @pytest.fixture(scope='package')
 def lvmoiscsi_device_config():
     return config.sr_device_config("LVMOISCSI_DEVICE_CONFIG")
@@ -17,8 +22,8 @@ def lvmoiscsi_sr(host, lvmoiscsi_device_config):
     sr.destroy()
 
 @pytest.fixture(scope='module')
-def vdi_on_lvmoiscsi_sr(lvmoiscsi_sr):
-    vdi = lvmoiscsi_sr.create_vdi('lvmoiscsi-VDI-test')
+def vdi_on_lvmoiscsi_sr(lvmoiscsi_sr: 'SR'):
+    vdi = lvmoiscsi_sr.create_vdi('lvmoiscsi-VDI-test', virtual_size='1GiB')
     yield vdi
     vdi.destroy()
 
