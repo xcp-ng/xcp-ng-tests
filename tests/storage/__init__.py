752611ea20c290c63268fac1baf7f2eb9692e76e
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2025-11-27 14:44:15 +0100

    storage: add utility function for vdi export import tests
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/__init__.py
+++ tests/storage/__init__.py
@@ -1,11 +1,13 @@
 from .storage import (
     CoalesceOperation,
+    ImageFormat,
     XVACompression,
     coalesce_integrity,
     cold_migration_then_come_back,
     install_randstream,
     live_storage_migration_then_come_back,
     try_to_create_sr_with_missing_device,
+    vdi_export_import,
     vdi_is_open,
     xva_export_import,
 )
