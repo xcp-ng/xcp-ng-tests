456c84577e975b53c3a0d8cdcf3a6ed5b74ab717
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-08 22:00:53 +0100

    tests/storage/largeblock: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/largeblock/conftest.py
+++ tests/storage/largeblock/conftest.py
@@ -11,11 +11,13 @@ from typing import TYPE_CHECKING, Generator
 if TYPE_CHECKING:
     from lib.host import Host
     from lib.sr import SR
+    from lib.vdi import VDI
+    from lib.vm import VM
 
 @pytest.fixture(scope='package')
 def largeblock_sr(host: Host,
                   unused_4k_disks: dict[Host, list[Host.BlockDeviceInfo]],
-                  image_format: ImageFormat) -> Generator[SR]:
+                  image_format: ImageFormat) -> Generator[SR, None, None]:
     """ A LARGEBLOCK SR on first host. """
     sr_disk = unused_4k_disks[host][0]["name"]
     sr = host.sr_create('largeblock', "LARGEBLOCK-local-SR-test",
@@ -26,13 +28,13 @@ def largeblock_sr(host: Host,
     sr.destroy()
 
 @pytest.fixture(scope='module')
-def vdi_on_largeblock_sr(largeblock_sr):
+def vdi_on_largeblock_sr(largeblock_sr: SR) -> Generator[VDI, None, None]:
     vdi = largeblock_sr.create_vdi('LARGEBLOCK-local-VDI-test')
     yield vdi
     vdi.destroy()
 
 @pytest.fixture(scope='module')
-def vm_on_largeblock_sr(host, largeblock_sr, vm_ref):
+def vm_on_largeblock_sr(host: Host, largeblock_sr: SR, vm_ref: str) -> Generator[VM, None, None]:
     vm = host.import_vm(vm_ref, sr_uuid=largeblock_sr.uuid)
     yield vm
     # teardown
