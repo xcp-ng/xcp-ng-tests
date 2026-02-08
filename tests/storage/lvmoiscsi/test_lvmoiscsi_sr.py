aad10a4f68a090f837430d70578a3bcc4b1cb1b3
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-08 22:19:01 +0100

    tests/storage/lvmoiscsi: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/lvmoiscsi/test_lvmoiscsi_sr.py
+++ tests/storage/lvmoiscsi/test_lvmoiscsi_sr.py
@@ -1,6 +1,7 @@
 import pytest
 
 from lib.common import vm_image, wait_for
+from lib.host import Host
 from lib.sr import SR
 from lib.vdi import VDI
 from lib.vm import VM
@@ -25,7 +26,7 @@ class TestLVMOISCSISRCreateDestroy:
     and VM import.
     """
 
-    def test_create_and_destroy_sr(self, host, lvmoiscsi_device_config):
+    def test_create_and_destroy_sr(self, host: Host, lvmoiscsi_device_config: dict[str, str]) -> None:
         # Create and destroy tested in the same test to leave the host as unchanged as possible
         sr = host.sr_create('lvmoiscsi', "lvmoiscsi-SR-test", lvmoiscsi_device_config, shared=True, verify=True)
         # import a VM in order to detect vm import issues here rather than in the vm_on_xfs_fixture used in
@@ -37,15 +38,15 @@ class TestLVMOISCSISRCreateDestroy:
 @pytest.mark.usefixtures("lvmoiscsi_sr")
 class TestLVMOISCSISR:
     @pytest.mark.quicktest
-    def test_quicktest(self, lvmoiscsi_sr):
+    def test_quicktest(self, lvmoiscsi_sr: SR) -> None:
         lvmoiscsi_sr.run_quicktest()
 
-    def test_vdi_is_not_open(self, vdi_on_lvmoiscsi_sr):
+    def test_vdi_is_not_open(self, vdi_on_lvmoiscsi_sr: VDI) -> None:
         assert not vdi_is_open(vdi_on_lvmoiscsi_sr)
 
     @pytest.mark.small_vm # run with a small VM to test the features
     @pytest.mark.big_vm # and ideally with a big VM to test it scales
-    def test_start_and_shutdown_VM(self, vm_on_lvmoiscsi_sr):
+    def test_start_and_shutdown_VM(self, vm_on_lvmoiscsi_sr: VM) -> None:
         vm = vm_on_lvmoiscsi_sr
         vm.start()
         vm.wait_for_os_booted()
@@ -53,7 +54,7 @@ class TestLVMOISCSISR:
 
     @pytest.mark.small_vm
     @pytest.mark.big_vm
-    def test_snapshot(self, vm_on_lvmoiscsi_sr):
+    def test_snapshot(self, vm_on_lvmoiscsi_sr: VM) -> None:
         vm = vm_on_lvmoiscsi_sr
         vm.start()
         try:
@@ -64,23 +65,23 @@ class TestLVMOISCSISR:
 
     @pytest.mark.small_vm
     @pytest.mark.parametrize("vdi_op", ["snapshot", "clone"])
-    def test_coalesce(self, storage_test_vm: 'VM', vdi_on_lvmoiscsi_sr: 'VDI', vdi_op: CoalesceOperation):
+    def test_coalesce(self, storage_test_vm: VM, vdi_on_lvmoiscsi_sr: VDI, vdi_op: CoalesceOperation) -> None:
         coalesce_integrity(storage_test_vm, vdi_on_lvmoiscsi_sr, vdi_op)
 
     @pytest.mark.small_vm
     @pytest.mark.parametrize("compression", ["none", "gzip", "zstd"])
-    def test_xva_export_import(self, vm_on_lvmoiscsi_sr: VM, compression: XVACompression):
+    def test_xva_export_import(self, vm_on_lvmoiscsi_sr: VM, compression: XVACompression) -> None:
         xva_export_import(vm_on_lvmoiscsi_sr, compression)
 
     @pytest.mark.small_vm
-    def test_vdi_export_import(self, storage_test_vm: VM, lvmoiscsi_sr: SR, image_format: ImageFormat):
+    def test_vdi_export_import(self, storage_test_vm: VM, lvmoiscsi_sr: SR, image_format: ImageFormat) -> None:
         vdi_export_import(storage_test_vm, lvmoiscsi_sr, image_format)
 
     # *** tests with reboots (longer tests).
 
     @pytest.mark.reboot
     @pytest.mark.small_vm
-    def test_reboot(self, host, lvmoiscsi_sr, vm_on_lvmoiscsi_sr):
+    def test_reboot(self, host: Host, lvmoiscsi_sr: SR, vm_on_lvmoiscsi_sr: VM) -> None:
         sr = lvmoiscsi_sr
         vm = vm_on_lvmoiscsi_sr
         host.reboot(verify=True)
