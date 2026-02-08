02f06a56b1504a404daa6c58a9a9a4ab817e075f
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-08 22:14:27 +0100

    tests/storage/lvm: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/lvm/conftest.py
+++ tests/storage/lvm/conftest.py
@@ -6,7 +6,8 @@ import logging
 
 from lib.host import Host
 from lib.sr import SR
-from lib.vdi import ImageFormat
+from lib.vdi import VDI, ImageFormat
+from lib.vm import VM
 
 from typing import Generator
 
@@ -14,7 +15,7 @@ from typing import Generator
 def lvm_sr(host: Host,
            unused_512B_disks: dict[Host, list[Host.BlockDeviceInfo]],
            image_format: ImageFormat
-           ) -> Generator[SR]:
+           ) -> Generator[SR, None, None]:
     """ An LVM SR on first host. """
     sr_disk = unused_512B_disks[host][0]["name"]
     sr = host.sr_create('lvm', "LVM-local-SR-test",
@@ -25,13 +26,13 @@ def lvm_sr(host: Host,
     sr.destroy()
 
 @pytest.fixture(scope='module')
-def vdi_on_lvm_sr(lvm_sr: SR):
+def vdi_on_lvm_sr(lvm_sr: SR) -> Generator[VDI, None, None]:
     vdi = lvm_sr.create_vdi('LVM-local-VDI-test')
     yield vdi
     vdi.destroy()
 
 @pytest.fixture(scope='module')
-def vm_on_lvm_sr(host, lvm_sr, vm_ref):
+def vm_on_lvm_sr(host: Host, lvm_sr: SR, vm_ref: str) -> Generator[VM, None, None]:
     vm = host.import_vm(vm_ref, sr_uuid=lvm_sr.uuid)
     yield vm
     # teardown
