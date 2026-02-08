7882178c77b3857ec43094cf85d2bcc5589186a5
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-08 22:32:47 +0100

    tests/storage/nfs: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/nfs/test_nfs_sr_crosspool_migration.py
+++ tests/storage/nfs/test_nfs_sr_crosspool_migration.py
@@ -1,5 +1,8 @@
 import pytest
 
+from lib.host import Host
+from lib.sr import SR
+from lib.vm import VM
 from tests.storage import cold_migration_then_come_back, live_storage_migration_then_come_back
 
 # Requirements:
@@ -17,9 +20,9 @@ from tests.storage import cold_migration_then_come_back, live_storage_migration_
 @pytest.mark.usefixtures('image_format')
 class Test:
     @pytest.mark.parametrize('dispatch_nfs', ['vm_on_nfs_sr', 'vm_on_nfs4_sr'], indirect=True)
-    def test_cold_crosspool_migration(self, host, hostB1, dispatch_nfs, local_sr_on_hostB1):
+    def test_cold_crosspool_migration(self, host: Host, hostB1: Host, dispatch_nfs: VM, local_sr_on_hostB1: SR) -> None:
         cold_migration_then_come_back(dispatch_nfs, host, hostB1, local_sr_on_hostB1)
 
     @pytest.mark.parametrize('dispatch_nfs', ['vm_on_nfs_sr', 'vm_on_nfs4_sr'], indirect=True)
-    def test_live_crosspool_migration(self, host, hostB1, dispatch_nfs, local_sr_on_hostB1):
+    def test_live_crosspool_migration(self, host: Host, hostB1: Host, dispatch_nfs: VM, local_sr_on_hostB1: SR) -> None:
         live_storage_migration_then_come_back(dispatch_nfs, host, hostB1, local_sr_on_hostB1)
