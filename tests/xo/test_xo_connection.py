2676b0befda09cbf63e1d9fe43527c734143b6ed
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-09 18:38:53 +0100

    tests/xo: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/xo/test_xo_connection.py
+++ tests/xo/test_xo_connection.py
@@ -1,12 +1,16 @@
 import logging
 
+from lib.host import Host
+
+from typing import List
+
 # This test is meant to test an XO connection.
 #
 # Requirements:
 # - an XCP-ng host >= 8.2 with latest updates.
 # - An XOA registered through `xo-cli`
 
-def test_xo_connection(hosts_with_xo):
+def test_xo_connection(hosts_with_xo: List[Host]) -> None:
     for h in hosts_with_xo:
         logging.debug(f"Testing connection for host: {h.hostname_or_ip}")
         assert h.xo_get_server_id() is not None
