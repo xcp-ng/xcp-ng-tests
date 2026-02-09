56cbe750b8cce6df1595f2cf10294edd5e74440c
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-02-09 09:59:03 +0100

    tests/xapi_plugins/plugin_lsblk: add type hints
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/xapi_plugins/plugin_lsblk/test_lsblk.py
+++ tests/xapi_plugins/plugin_lsblk/test_lsblk.py
@@ -1,6 +1,8 @@
+from lib.host import Host
+
 # Requirements:
 # From --hosts parameter:
 # - host(A1): first XCP-ng host > 8.2.
 
-def test_list_block_devices(host):
+def test_list_block_devices(host: Host) -> None:
     host.call_plugin('lsblk.py', 'list_block_devices')
