b5165051411fa2975b10c2356cd8f493419117bf
Yann Sionneau <yann.sionneau@vates.tech>
2026-02-12 18:11:41 +0100

    improve performance and add usage and skeleton of -m option

--- scripts/list.sh
+++ scripts/list.sh
@@ -15,8 +15,28 @@
 show_by_features="no"
 FEATURES=""
 TEST=""
+CHECK_MISSING="no"
 
-while getopts ":f:t:" o; do
+usage() {
+	echo "Usage: list.sh [-m] [-f <FEATURE_LIST>] [-t <TEST_NAME>]"
+	echo
+	echo "Options:"
+	echo -e "\t-m\t\t\tPrint all tests that are missing docstring metadata"
+	echo -e "\t-f <FEATURE_LIST>\tLists all tests that are testing a feature from the supplied FEATURE_LIST."
+	echo -e "\t-t <TEST_NAME>\t\tShow docstring metadata of TEST_NAME: a description of what the test does"
+	echo -e "\t\t\t\tand the list of tested features."
+	echo
+	echo "Examples:"
+	echo 'List all tests that are testing the `migration` feature:'
+	echo -e "\tlist.sh -f migration"
+	echo 'List all tests that are testing `migration` or `cross-pool-migration` feature:'
+	echo -e "\tlist.sh -f \"migration cross-pool-migration\""
+	echo 'Show what the `test_cross_pool_migration` test is testing:'
+	echo -e "\tlist.sh -t test_cross_pool_migration"
+
+}
+
+while getopts ":f:t:m" o; do
 	case "${o}" in
 		f)
 			FEATURES=${OPTARG}
@@ -26,16 +46,34 @@ while getopts ":f:t:" o; do
 		t)
 			TEST=${OPTARG}
 			;;
+		m)
+			CHECK_MISSING="yes"
+			;;
 		*)
 			usage
+			exit 1
 			;;
 	esac
 done
 shift $((OPTIND-1))
 
+if [ "${CHECK_MISSING}" == "yes" ]
+then
+	TMP=$(mktemp)
+	uv run pytest --collect-only --verbose > ${TMP}
+	tests=$(sed '/\s<Function/!d; s/\s*<Function \(.*\)>/\1/g' ${TMP})
+	echo Checking
+	for test in ${tests}
+	do
+		_test=$(printf %q "${test}")
+	done
+	rm ${TMP}
+	exit
+fi
+
 if [ "${TEST}" != "" ]
 then
-	uv run pytest --collect-only --verbose -k ${TEST} | sed "/\s*<Function ${TEST}[\[>]/,\$!d" | sed "/\s*<Function ${TEST}[\[>]/,/\s*<Function/!d" | sed "/\s*<Function ${TEST}[^\[>]/d" | grep -v "====="
+	uv run pytest --collect-only --verbose -k "${TEST}" | sed "/\s*<Function ${TEST}[\[>]/,\$!d" | sed '/^$/,/====/d' | sed "/\s*<Function ${TEST}[\[>]/,/\s*<Function/!d" | sed "/\s*<Function ${TEST}[^\[>]/d" | grep -v "====="
 	exit
 fi
 
@@ -54,8 +92,7 @@ tests=$(sed '/\s<Function/!d; s/\s*<Function \(.*\)>/\1/g' ${TMP})
 
 for test in ${tests}
 do
-#	echo "Test ${test}:"
-	_test=$(echo $test | sed 's@\[@\\\[@g; s@\]@\\\]@g')
+	_test=$(printf %q "${test}")
 	features=$(sed '/<Function '"${_test}"'/,/<Function/!d; /<Function '"${_test}"'/,/features: /!d; /features:/!d; s/\s*features://g' ${TMP})
 	for searched_feature in ${FEATURES}
 	do
@@ -63,11 +100,11 @@ do
 		do
 			if [[ "${feature}" == "${searched_feature}" ]]
 			then
-				echo ${test} | sed 's/\[.*\]//g'
+				echo "${test//\[*\]/}"
 				break 2
 			fi
 		done
 	done
 done
 
-#rm ${TMP}
+rm ${TMP}
