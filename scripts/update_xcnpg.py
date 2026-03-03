d0b5ad876b8dd0620da903c5aed3422829028938
Olivier Hoareau <olivier.hoareau@vates.tech>
2026-03-03 16:11:09 +0100

    fix: changes flake8 failed lint
    Signed-off-by: Olivier Hoareau <olivier.hoareau@vates.tech>

--- scripts/update_xcnpg.py
+++ scripts/update_xcnpg.py
@@ -6,10 +6,11 @@ import os
 import sys
 import threading
 
-sys.path.append(f"{os.path.abspath(os.path.dirname(__file__))}/..") # noqa
-from lib.commands import ssh, SSHCommandFailed
+from lib.commands import SSHCommandFailed, ssh
 from lib.common import is_uuid
 from lib.pool import Pool
+
+sys.path.append(f"{os.path.abspath(os.path.dirname(__file__))}/..") # noqa
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
@@ -33,7 +34,7 @@ def is_new_host_ready(ip_address):
     except Exception:
         return False
 
-def  install_updates_reboot(host):
+def install_updates_reboot(host):
     try:
         logging.info('Running updates on the host:' + str(host))
         # host.install_updates()
@@ -42,7 +43,7 @@ def  install_updates_reboot(host):
         # host.reboot()
         # wait_for(lambda: not os.system(f"nc -zw5 {host} 22"),
         #              "Wait for ssh up on host", timeout_secs=10 * 60, retry_delay_secs=5)
-        return(True)
+        return True
     except Exception:
         return False
 
@@ -59,7 +60,7 @@ def main():
     # TODO : How to manage and validate IPv6?
 
     # verify that the host is available (ssh) and that it is indeed the master of a pool
-    # TODO: Exit the script if there's a problem, or confirm that it's working correctly.
+    # TODO: Exit the script if there's a problem, or confirm that it's working correctly.
 
     # TODO: replace with an existing function in host or pool
     if not is_ssh_up(args.host):
@@ -67,7 +68,7 @@ def main():
 
     try:
         pool = Pool(args.host) # will fail if host is not XCP-ng or XAPI doesn't respond yet
-    except Exception as e:
+    except Exception:
         raise Exception(f"Host `{args.host}` isn't ready or isn't an XCP-ng host")
 
     host = pool.master
@@ -77,7 +78,7 @@ def main():
 
     # We build a list of secondaries, if they exist because we will run updates and reboots in parallel on these,
     # once the primary one is done and available.
-    slaves_list = pool.hosts[1:] 
+    slaves_list = pool.hosts[1:]
     logging.info('Here is the slaves list: %s', slaves_list)
 
     # TODO: Adding a feature/setting to add/enable repos if requested?
@@ -95,28 +96,28 @@ def main():
     mupdates = install_updates_reboot(host)
     if not mupdates:
         logging.info('Errors durring the update process on the master: ' + str(host))
-        return        
+        return
 
     logging.info('How many secondaries? ' + str(len(slaves_list)))
 
     # Management of secondary servers, if any.
-    if len(slaves_list) == 0 :
+    if len(slaves_list) == 0:
         logging.info('No other servers to process in the pool.')
         return
-    
-    if len(slaves_list) == 1 :
+
+    if len(slaves_list) == 1:
         print(slaves_list[0])
         logging.info('ONE SERVER TO DO')
         # logging.info(slaves_list[0].install_updates())
         # slaves_list[0].reboot()
         install_updates_reboot(host)
-    elif len(slaves_list) > 1 :
+    elif len(slaves_list) > 1:
         logging.info('MULTIPLE SERVERS TO DO')
         # Several secondary processes. We're using multithreading for updates and reboots, all at the same time.
         threads = []
         for secondary in slaves_list:
             # Add function in the thread below where the desired secondary is passed.
-            # t = threading.Thread(target=print(secondary))
+            # t = threading.Thread(target=print(secondary))
             t = threading.Thread(target=install_updates_reboot, args=(secondary,))
             threads.append(t)
 
@@ -126,13 +127,14 @@ def main():
         for t in threads:
             t.join()
 
-
     # TODO: when everyone is up to date, say it
 
-    # TODO: 
+    # TODO:
     # For the VMs, we want to take snapshots at the end.
-    # How do we know where the VM is? Pass the host as a parameter? So add two parameters, one of which is optional for the VMs?
-    # We need to know if the VM and if the VM will take a snapshot. But we need to know the VM's UUID and the host's IP for it to take the snapshot.
+    # How do we know where the VM is? Pass the host as a parameter?
+    #     So add two parameters, one of which is optional for the VMs?
+    # We need to know if the VM and if the VM will take a snapshot.
+    # But we need to know the VM's UUID and the host's IP for it to take the snapshot.
     # Local file for testing, read the netbox (to be updated?), read the GRIST table?
 
 if __name__ == '__main__':
