3613f0fa6e0c099d08ac763e0201ea3552bd2f1d
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-04-22 21:40:16 +0200

    migration: create a xfs sr on the second host for intrapool migration
    in order to have large enough disks to migrate VMs with large disks.
    We should think of a better way to say 'I need a local SR with 16TiB available'.
    
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/nfs/test_nfs_sr_intrapool_migration.py
+++ tests/storage/nfs/test_nfs_sr_intrapool_migration.py
@@ -14,7 +14,7 @@ from tests.storage import cold_migration_then_come_back, live_storage_migration_
 
 @pytest.mark.small_vm # run with a small VM to test the features
 @pytest.mark.big_vm # and ideally with a big VM to test it scales
-@pytest.mark.usefixtures("hostA2", "local_sr_on_hostA2")
+@pytest.mark.usefixtures("hostA2", "xfs_sr_on_hostA2")
 # Make sure these fixtures are called before the parametrized one
 @pytest.mark.usefixtures('vm_ref')
 @pytest.mark.usefixtures('image_format')
@@ -25,9 +25,9 @@ class Test:
         live_storage_migration_then_come_back(dispatch_nfs, host, hostA2, sr)
 
     @pytest.mark.parametrize('dispatch_nfs', ['vm_on_nfs_sr', 'vm_on_nfs4_sr'], indirect=True)
-    def test_cold_intrapool_migration(self, host: Host, hostA2: Host, dispatch_nfs: VM, local_sr_on_hostA2: SR) -> None:
-        cold_migration_then_come_back(dispatch_nfs, host, hostA2, local_sr_on_hostA2)
+    def test_cold_intrapool_migration(self, host: Host, hostA2: Host, dispatch_nfs: VM, xfs_sr_on_hostA2: SR) -> None:
+        cold_migration_then_come_back(dispatch_nfs, host, hostA2, xfs_sr_on_hostA2)
 
     @pytest.mark.parametrize('dispatch_nfs', ['vm_on_nfs_sr', 'vm_on_nfs4_sr'], indirect=True)
-    def test_live_intrapool_migration(self, host: Host, hostA2: Host, dispatch_nfs: VM, local_sr_on_hostA2: SR) -> None:
-        live_storage_migration_then_come_back(dispatch_nfs, host, hostA2, local_sr_on_hostA2)
+    def test_live_intrapool_migration(self, host: Host, hostA2: Host, dispatch_nfs: VM, xfs_sr_on_hostA2: SR) -> None:
+        live_storage_migration_then_come_back(dispatch_nfs, host, hostA2, xfs_sr_on_hostA2)
