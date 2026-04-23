e0dd6d16aad0cc9902aa94afce95d41e30590795
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-04-23 11:09:07 +0200

    migration: remove redudant usefixtures
    They are already explicitly used in the test parameters
    
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- tests/storage/lvmohba/test_lvmohba_sr_intrapool_migration.py
+++ tests/storage/lvmohba/test_lvmohba_sr_intrapool_migration.py
@@ -11,7 +11,6 @@ from tests.storage import cold_migration_then_come_back, live_storage_migration_
 
 @pytest.mark.small_vm # run with a small VM to test the features
 @pytest.mark.big_vm # and ideally with a big VM to test it scales
-@pytest.mark.usefixtures("hostA2", "xfs_sr_on_hostA2")
 @pytest.mark.thick_provisioned
 class Test:
     def test_live_intrapool_shared_migration(self, host, hostA2, vm_on_lvmohba_sr):
--- tests/storage/lvmoiscsi/test_lvmoiscsi_sr_crosspool_migration.py
+++ tests/storage/lvmoiscsi/test_lvmoiscsi_sr_crosspool_migration.py
@@ -14,7 +14,6 @@ from tests.storage import cold_migration_then_come_back, live_storage_migration_
 
 @pytest.mark.small_vm # run with a small VM to test the features
 @pytest.mark.big_vm # and ideally with a big VM to test it scales
-@pytest.mark.usefixtures("hostB1", "xfs_sr_on_hostB1")
 @pytest.mark.thick_provisioned
 class Test:
     def test_cold_crosspool_migration(
