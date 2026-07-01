6efc91bcf63845856b47e80575c60710b55d85c5
Vincent Michel <vincent.michel@vates.tech>
2026-07-01 17:14:16 +0200

    Hotfix for handling ipv6 in temp_large_dir fixture
    Signed-off-by: Vincent Michel <vincent.michel@vates.tech>

--- tests/storage/conftest.py
+++ tests/storage/conftest.py
@@ -34,7 +34,10 @@ def temp_large_dir(host: Host) -> Generator[str, None, None]:
         mount_path = f'/mnt/nfs_{temp_id}'
         host.ssh(f'mkdir -p {mount_path}')
         path = f'{mount_path}/temp_large_dir_{temp_id}'
-        host.ssh(f'mount -t nfs {nfs_conf["server"]}:{nfs_conf["serverpath"]} {mount_path}')
+        hostname = nfs_conf["server"]
+        if ":" in hostname:
+            hostname = f"[{hostname}]"
+        host.ssh(f'mount -t nfs {hostname}:{nfs_conf["serverpath"]} {mount_path}')
         host.ssh(f'mkdir {path}')
         yield path
         host.ssh(f'rm -rf {path}')
