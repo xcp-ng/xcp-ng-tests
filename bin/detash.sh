7bb0cc1118de370c3f7576868ee3467e58d851b1
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2026-07-23 11:18:06 +0200

    Use teetty instead of tee for debug session logging
    teetty preserves interactive TTY features (like pdb) while still
    capturing output to a log file.
    
    Related-to: XCPNG-2526
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>
    (cherry picked from commit f45395264f6aca8f9ebd1970c706fa37f0992d73)

--- bin/detash.sh
+++ bin/detash.sh
@@ -144,7 +144,7 @@ EOF
     echo "$status" > "$statusfile"
     cmdline=$(printf '"%s" ' "$@")  # Escape quotes
     [ "" != "$cmdline" ] || cmdline="set"
-    cmdline="status=0 ; $cmdline 2>&1 | tee --ignore-interrupts \"$logfile\" || status=\$? ; \
+    cmdline="status=0 ; teetty --out \"$logfile\" -- $cmdline || status=\$? ; \
      echo \${status} > \"$statusfile\" ; \
      sleep 1"  # Log cmdline output unbuffered and forward status to quit_
 
