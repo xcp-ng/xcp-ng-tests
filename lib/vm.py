e5bcfb834c7c8642ac56968b8906e5d38b3998bb
Vincent Michel <vincent.michel@vates.tech>
2026-09-11 16:07:59 +0200

    Fix a race condition when importing a VM from the cache
    When two jobs import the same VM, it's possible for the cloned
    VM of the first job to be incorrectly detected as a cached VM by
    the second one, due to the cache key in the description not being
    cleared during the clone, but slightly after. This is fixed by
    resetting the description during the clone operation using the
    `new-name-description` parameter.
    
    Signed-off-by: Vincent Michel <vincent.michel@vates.tech>

--- lib/vm.py
+++ lib/vm.py
@@ -707,11 +707,14 @@ class VM(BaseVM):
         logging.info("New VBD %s", vbd_uuid)
         return vbd
 
-    def clone(self, *, name: str | None = None) -> "VM":
+    def clone(self, *, name: str | None = None, description: str | None = None) -> VM:
         if name is None:
             name = self.name() + '_clone_for_tests'
         logging.info("Clone VM")
-        uuid = self.host.xe('vm-clone', {'uuid': self.uuid, 'new-name-label': name})
+        values: XeParams = {'uuid': self.uuid, 'new-name-label': name}
+        if description is not None:
+            values['new-name-description'] = description
+        uuid = self.host.xe('vm-clone', values)
         return VM(uuid, self.host)
 
     def set_variable_from_file(
