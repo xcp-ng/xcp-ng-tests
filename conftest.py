c1cc9a72dcabd01fe69635c0fcac72af8700531c
Vincent Michel <vincent.michel@vates.tech>
2026-09-15 13:30:12 +0200

    Add a specialized `vm.clone_cached_vm` method
    Having a dedicated method for cloning a cached VM is useful, as it
    as it allows for properly handling its description which is where
    the cache key is stored. This way, the caller does not have this
    responsibility. Note that the suffix added to the base VM name is
    now `_clone_from_cache` when this method is used, while it remains
    `_clone_for_tests` in the general case.
    
    Signed-off-by: Vincent Michel <vincent.michel@vates.tech>

--- conftest.py
+++ conftest.py
@@ -645,7 +645,7 @@ def imported_vm(host: Host, vm_ref: str) -> Generator[VM, None, None]:
     if CACHE_IMPORTED_VM:
         # Clone the VM before running tests, so that the original VM remains untouched
         logging.info(">> Clone cached VM before running tests")
-        vm = vm_orig.clone(description="")
+        vm = vm_orig.clone_cached_vm()
     else:
         vm = vm_orig
 
@@ -819,9 +819,7 @@ def _vm_from_cache(
 
     # Clone the VM before running tests, so that the original VM remains untouched
     logging.info("Cloning VM from cache")
-    vm = base_vm.clone(name=prefix_object_name(_vm_name(request, vm_def)))
-    # Remove the description, which may contain a cache identifier
-    vm.param_set('name-description', "")
+    vm = base_vm.clone_cached_vm(name=prefix_object_name(_vm_name(request, vm_def)))
 
     vms.append(vm)
 
